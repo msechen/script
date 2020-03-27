580a69bfd8b0f1338e01a8547f438a713d2bf38e
kolly <kolly.li@klook.com>
2020-03-27 08:20:04 +0800

    优化自动加好友功能

--- service/user_service.py
+++ service/user_service.py
@@ -1,11 +1,20 @@
+import sys
+
+from sqlalchemy.exc import DatabaseError
+
 from dao import user_dao
 from modal import *
 
 
 def add_new_user(puid, nickname, avatar, sex, city):
-    print(puid, nickname, avatar, sex, city)
+    print('add_new_user：', puid, nickname, avatar, sex, city)
     u = User(puid, nickname, avatar, sex, city)
-    return user_dao.add_user(u)
+    try:
+        return user_dao.add_user(u)
+    except DatabaseError as err:
+        print("DatabaseError: {0}".format(err))
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
 
 
 def query_user():
@@ -13,5 +22,5 @@ def query_user():
 
 
 if __name__ == "__main__":
-    user_id = add_new_user("1234567890", "test", "http://11111.jpg", "male", "sz")
+    user_id = add_new_user("1234567890", "kolly🤔", "http://11111.jpg", 'wo', "sz")
     print(user_id)
