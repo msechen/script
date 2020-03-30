be3d1c2545c51b73fa703b197040112fff1aec52
kolly <kolly.li@klook.com>
2020-03-30 17:05:44 +0800

    log bug

--- jobs/job.py
+++ jobs/job.py
@@ -121,7 +121,7 @@ def credit_card_repay():
 def send_service_info(service_id, info):
     service = service_dao.query_service_by_id(service_id)
     for sub in subscribe_dao.query_subscribe_by_service_id(service_id):
-        logger.info("用户{}订阅「{}」".format(service.name, sub.user_id))
+        logger.info("用户{}订阅「{}」".format(sub.user_id, service.name))
         user = user_dao.query_user_by_id(sub.user_id)
         logger.info("用户{}昵称：{}".format(user.id, user.nickname))
         chat = ensure_one(bot.friends(update=True).search(user.nickname + '-' + str(user.id)))
