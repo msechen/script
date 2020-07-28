83f4e0685297dc7615c1a927ade4270b8e4d2f50
kolly <kolly.li@klook.com>
2020-07-28 15:33:51 +0800

    完成知乎 google sheet 数据定时同步

--- common/google_sheet.py
+++ common/google_sheet.py
@@ -25,11 +25,12 @@ def get_sheet():
     global sheet
 
     creds = None
+    module_path = os.path.dirname(__file__)
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists(module_path + '/token.pickle'):
+        with open(module_path + '/token.pickle', 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -37,10 +38,10 @@ def get_sheet():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                module_path + '/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open(module_path + '/token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('sheets', 'v4', credentials=creds)
