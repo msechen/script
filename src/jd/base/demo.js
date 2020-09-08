cefad05fe27cd1fadd472d9670a644ebb53e68ef
jyhuang <326127285@qq.com>
2020-09-08 18:08:30 +0800

    perf(jd/base): 增加 Base 类

---
+++ src/jd/base/demo.js
@@ -0,0 +1,30 @@
+const Base = require('./index');
+
+const {sleep} = require('../../lib/common');
+
+class Demo extends Base {
+  static scriptName = 'Demo';
+  static apiOptions = {
+    options: {
+      headers: {
+        'User-Agent': 'jdapp',
+      },
+    },
+    formatDataFn: data => data,
+  };
+  static apiExtends = {
+    requestFnName: 'doFormBody',
+    apiNames: ['apiName'],
+  };
+
+  static async doMain(api, shareCodes) {
+    this.log('1');
+    await sleep(2);
+    this.log('2');
+    await sleep(2);
+    this.log('3');
+    api.apiName();
+  }
+}
+
+module.exports = Demo;
---
+++ src/jd/base/index.js
@@ -0,0 +1,66 @@
+const _ = require('lodash');
+
+const {Request} = require('../api');
+const {sleep, getNowMoment} = require('../../lib/common');
+const {printLog} = require('../../lib/common');
+
+class Base {
+  static _ = _;
+  // 脚本名称
+  static scriptName = 'scriptName';
+  // 循环次数
+  static times = 1;
+  // request 参数
+  static apiOptions = {
+    signData: {},
+    options: {},
+    formatDataFn: void 0,
+  };
+  // aoi 参数
+  static apiExtends = {
+    // 具体请求 functionName
+    requestFnName: 'doFormBody',
+    // 接口名称
+    apiNames: [],
+  };
+
+  static async doMain(api, shareCodes) {
+  }
+
+  // helpers
+  static log(output) {
+    printLog(this.scriptName, void 0, output);
+  }
+
+  static getNowHour() {
+    return getNowMoment().hours();
+  }
+
+  static initApi(cookie) {
+    const {signData, options, formatDataFn} = this.apiOptions;
+    const {requestFnName, apiNames} = this.apiExtends;
+    const api = new Request(cookie, signData, options, formatDataFn);
+    if (requestFnName) {
+      for (const apiName of apiNames) {
+        api[apiName] = api[requestFnName].bind(api, apiName);
+      }
+    }
+    return api;
+  }
+
+  static async init(cookie, shareCodes) {
+    const api = this.initApi(cookie);
+    await this.doMain(api, shareCodes);
+  }
+
+  static async start(data) {
+    for (let i = 0; i < this.times; i++) {
+      for (const {cookie, shareCodes} of data) {
+        await this.init(cookie, i === 0 ? shareCodes : void 0);
+      }
+      await sleep(2);
+    }
+  }
+}
+
+module.exports = Base;
