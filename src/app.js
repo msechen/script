4bf4837ed3cf28dbcbd2ff1e942ec836d425a6d8
jyhuang <326127285@qq.com>
2020-09-11 12:51:58 +0800

    update(app): 下线 BuildingShop 脚本

--- src/app.js
+++ src/app.js
@@ -13,7 +13,6 @@ const cash = require('./jd/cash');
 const bean = require('./jd/bean');
 const invitation = require('./jd/invitation');
 const superMarket = require('./jd/superMarket');
-const BuildingShop = require('./jd/buildingShop');
 
 const getCookieData = (name, shareCode) => {
   shareCode && (shareCode = [].concat(shareCode));
@@ -55,7 +54,6 @@ async function main() {
         await runScript(bean, 'bean');
         await runScript(invitation, 0);
         await runScript(superMarket, 0);
-        await BuildingShop.start(getCookieData());
       },
     },
     {
