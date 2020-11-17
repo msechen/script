b499eff6d950c1baefe394ab9cdad39ef639fcbf
jyhuang <326127285@qq.com>
2020-11-17 15:20:23 +0800

    update(jd/pet): 增加任务和调整日志输出

--- src/jd/newPet/index.js
+++ src/jd/newPet/index.js
@@ -21,6 +21,7 @@ class Pet extends Base {
       'slaveHelp', /* 助力 */
       'getSingleShopReward', /* 浏览店铺 */
       'getThreeMealReward', /* 三餐签到 */
+      'taskInit', /* 任务列表 */
     ],
   };
 
@@ -28,32 +29,38 @@ class Pet extends Base {
     return this._.property('resultCode')(data) === '0';
   }
 
+  static logReward(data) {
+    this.isSuccess(data) && this.log(`获取到狗粮: ${data.result.threeMealReward || data.result.reward}`);
+  }
+
   static async doMain(api, shareCodes) {
+    const self = this;
+    const _ = this._;
+
     for (const shareCode of shareCodes) {
       await api.slaveHelp({shareCode}).then(data => {
-        const {resultCode, result} = data;
-        if (resultCode === '0') {
-          this.log(`给 ${result.masterNickName || 'unknown'} 助力成功`);
+        if (self.isSuccess(data)) {
+          this.log(`给 ${_.property('result.masterNickName')(data) || 'unknown'} 助力成功`);
         } else {
           this.log(data.message);
         }
       });
     }
 
-    await api.getSingleShopReward({version: 1, index: 2, type: 1}).then(async data => {
-      if (!this.isSuccess(data)) return;
-      await sleep(2);
-      await api.getSingleShopReward({version: 1, index: 2, type: 2});
-    });
+    for (let index = 2; index < 5; index++) {
+      await api.getSingleShopReward({version: 1, index, type: 1}).then(async data => {
+        if (!self.isSuccess(data)) return;
+        await sleep(2);
+        await api.getSingleShopReward({version: 1, index, type: 2}).then(self.logReward.bind(self));
+      });
+    }
   }
 
   static async doCron(api) {
     const self = this;
     const _ = this._;
 
-    await api.getThreeMealReward().then(data => {
-      self.log(`领取到狗粮: ${data.result.threeMealReward}`);
-    });
+    await api.getThreeMealReward().then(self.logReward.bind(self));
   }
 }
 
