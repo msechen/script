0ef1d7ce4bf7f91d6f18de1ae18094480571e335
jyhuang <326127285@qq.com>
2021-05-18 17:19:42 +0800

    perf(jd/LiveRedEnvelopeRain): 改进请求逻辑

--- src/jd/live/RedEnvelopeRain.js
+++ src/jd/live/RedEnvelopeRain.js
@@ -1,10 +1,12 @@
 const Template = require('../base/template');
 
 const {sleep, writeFileJSON, singleRun, parallelRun} = require('../../lib/common');
-const {sleepTime} = require('../../lib/cron');
-const {getMoment} = require('../../lib/moment');
+const {sleepDate} = require('../../lib/cron');
+const {getMoment, getNowDate} = require('../../lib/moment');
 const {getEnv} = require('../../lib/env');
 
+// 以本地文件数据为准, 每次都需要清空
+writeFileJSON([], '../../../charles/form/jd/liveActivityV946.json', __dirname);
 const {live} = require('../../../charles/api');
 
 class LiveRedEnvelopeRain extends Template {
@@ -24,6 +26,44 @@ class LiveRedEnvelopeRain extends Template {
     async function handleRain() {
       const hadRainAreas = _.uniqBy(_.filter(await getRainArea()), 'actId');
 
+      const hours = [];
+      // 京豆雨一般从9点开始
+      for (let i = 8; i <= 24; i++) {
+        hours.push(i);
+      }
+      const nowHour = self.getNowHour();
+      let hour;
+      for (let i = 0; i < hours.length; i++) {
+        const prev = i - 1;
+        if (prev < 0) continue;
+        if (nowHour < hours[i] && nowHour >= (hours[prev] || 0)) {
+          hour = hours[i];
+          break;
+        }
+      }
+      if (hour && _.isEmpty(hadRainAreas)) {
+        const startTime = `${getNowDate()} ${hour}:00:00`;
+        [
+          'RRA4Kr8hch51J49qasBvEv8Agf8chAD', // 5.18 17
+          // 'RRA2u4bEZ2zLYr9PKfwqfXngbVCmqNE', // 5.18 16
+          // 'RRA4RhWMc159kA62qLbaEa88evE7owb', // 5.17
+          // 'RRA3mYmDhvzgxdpAJuscqHt32VNDqCM', // 5.16
+          // 'RRA2cUocg5uYEyuKpWNdh4qE4NW1bN2', // 5.15
+          // 'RRAKoqJYHaBPhFyuYdunFqtjeVBLDC', // 5.14 半点
+          // 'RRA3zxrJChU14WAc5ZCD4GQMSxDYLRC', // 5.14
+          // 'RRA3mYmDhvzgxdpAJuscqHt32VNDqCM', // 5.13
+          // 'RRA2cUocg5uYEyuKpWNdh4qE4NW1bN2', // 5.12
+          // 'RRA3zxrJChU14WAc5ZCD4GQMSxDYLRC', // 5.11
+          // 'RRA4RhWMc159kA62qLbaEa88evE7owb', // 5.10
+          // 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', // 5.10
+        ].forEach((actId, index) => {
+          hadRainAreas.push({
+            actId,
+            startTime,
+            liveId: `liveId${index}`,
+          });
+        });
+      }
       if (_.isEmpty(hadRainAreas)) return;
 
       await parallelRun({
@@ -59,10 +99,11 @@ class LiveRedEnvelopeRain extends Template {
     async function waitForLottery(targetIconArea) {
       const {startTime, endTime, actId, liveId} = targetIconArea;
       const targetMoment = getMoment(startTime);
-      const logMsg = `${liveId} 下一场红包雨(${actId}): ${targetMoment.format('YYYY-MM-DD HH:mm:ss')}`;
+      const logMsg = `${liveId} 下一场红包雨(${actId}): ${targetMoment.format()}`;
       api.log(logMsg);
       (api.currentCookieTimes === 0) && console.log(`[${self.scriptNameDesc}] ${logMsg}`);
-      await sleepTime([targetMoment.hour(), targetMoment.minute(), 10]);
+      targetMoment.add(10, 'second');
+      await sleepDate(targetMoment.format());
       await noahRedRainLottery(actId);
     }
 
--- src/lib/moment.js
+++ src/lib/moment.js
@@ -1,15 +1,19 @@
 /**
  * @description 时间/时区相关操作
  */
+const FORMAT_FULL_DATE = 'YYYY-MM-DD HH:mm:ss';
 
 const moment = require('moment-timezone');
+
+// 定义默认format
+moment.defaultFormat = FORMAT_FULL_DATE;
 
 // TODO 更改为 moment.tz.setDefault
 const getMoment = (date = void 0, tz = 'Asia/Shanghai') => moment.tz(date, tz);
 const getNowDate = (format = 'YYYY-MM-DD') => getMoment().format(format);
 const getNowHour = () => getMoment().hour();
 const getNowTime = getNowDate.bind(0, 'HH:mm:ss');
-const getFullDate = getNowDate.bind(0, 'YYYY-MM-DD HH:mm:ss');
+const getFullDate = getNowDate.bind(0, FORMAT_FULL_DATE);
 
 module.exports = {
   getMoment,
