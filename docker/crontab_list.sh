97bde4db001a449b99669d5af450fea311540949
ZFeng3242 <pl8354629pou@163.com>
2021-06-15 23:55:21 +0800

    新增开卡脚本，不使用请尽快注释，修复金币收集火爆

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -62,9 +62,13 @@
 #618母婴夺宝大作战
 15 9,21 9-20 6 * node /scripts/jd_618Opencard06.js >> /scripts/logs/jd_618Opencard06.log 2>&1
 #大牌联合'臻'宠粉丝
-15 8,20 9-20 6 * node /scripts/jd_618Opencard07.js >> /scripts/logs/jd_618Opencard07.log 2>&1
+15 8,20 9-23 6 * node /scripts/jd_618Opencard07.js >> /scripts/logs/jd_618Opencard07.log 2>&1
 # 618大牌联合 年中钜惠购
-01 8,20 9-20 6 * node /scripts/jd_618Opencard08.js >> /scripts/logs/jd_618Opencard08.log 2>&1
+01 8,20 9-23 6 * node /scripts/jd_618Opencard08.js >> /scripts/logs/jd_618Opencard08.log 2>&1
+# 大牌超级联合 618年中购
+01 3,13 9-23 6 * node /scripts/jd_618Opencard09.js >> /scripts/logs/jd_618Opencard09.log 2>&1
+# 大牌联合618年中钜惠抢先购
+01 2,12 9-23 6 * node /scripts/jd_618Opencard10.js >> /scripts/logs/jd_618Opencard10.log 2>&1
 #限时盲盒
 30 7,19 1-18 6 * node /scripts/jd_limitBox.js >> /scripts/logs/jd_limitBox.log 2>&1
 #主会场红包雨
