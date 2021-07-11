04fb597b76f358eb2ecd40eb511fafcd9389cd3a
drew <68432965+drew0531@users.noreply.github.com>
2021-07-11 09:28:53 +0800

    Update jup.sh

--- jup.sh
+++ jup.sh
@@ -4,7 +4,7 @@
 dir_shell=$(dirname $(readlink -f "$0"))
 dir_root=$dir_shell
 url_shell=${https://github.com/drew0531/tsnode}
-url_scripts=${JD_SCRIPTS_URL:-git@jd_scripts_gitee:lxk0301/jd_scripts.git}
+url_scripts=${https://github.com/chinnkarahoi/jd_scripts.git}
 send_mark=$dir_shell/send_mark
 
 ## 导入通用变量与函数
