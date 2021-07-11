fe2c6ccb23bb8c8e60670fd1fba93fd3cdbb48ff
ZFeng3242 <pl8354629pou@163.com>
2021-07-11 23:38:53 +0800

    fix

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -100,7 +100,6 @@ if ($.isNode()) {
 
 async function jdDreamFactory() {
   try {
-    await Getusertoken();
     await userInfo();
     await QueryFriendList();//查询今日招工情况以及剩余助力次数
     // await joinLeaderTuan();//参团
