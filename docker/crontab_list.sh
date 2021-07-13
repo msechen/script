a1638f5997b9b1a65906b1894abe82c89008214d
ZFeng3242 <pl8354629pou@163.com>
2021-07-14 01:32:31 +0800

    删除过期脚本

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -33,8 +33,6 @@
 47 10 * * * node /scripts/jd_djjl.js >> /scripts/logs/jd_djjl.log 2>&1
 #星系牧场
 03 */2 * * * node /scripts/jd_qqxing.js >> /scripts/logs/jd_qqxing.log 2>&1
-#欧洲杯
-0 10 * * * node /scripts/jd_europeancup.js >> /scripts/logs/jd_europeancup.log 2>&1
 #食力街跑酷达人
 10 11 * * * node /scripts/jd_paoku.js >> /scripts/logs/jd_paoku.log 2>&1
 #天天优惠大乐透
