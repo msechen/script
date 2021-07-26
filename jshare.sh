f41a3e3fd2cc9367a0a7860f3edfcb6cdb764f72
drew <68432965+drew0531@users.noreply.github.com>
2021-07-26 15:23:32 +0800

    Update jshare.sh

--- jshare.sh
+++ jshare.sh
@@ -294,15 +294,3 @@ update_crontab () {
         crontab $list_crontab_user
     fi
 }
-
-## 生成pt_pin清单
-gen_pt_pin_array () {
-    local tmp1 tmp2 i pt_pin_temp
-    for ((user_num=1; user_num<=$user_sum; user_num++)); do
-        tmp1=Cookie$user_num
-        tmp2=${!tmp1}
-        i=$(($user_num - 1))
-        pt_pin_temp=$(echo $tmp2 | perl -pe "{s|.*pt_pin=([^; ]+)(?=;?).*|\1|; s|%|\\\x|g}")
-        [[ $pt_pin_temp == *\\x* ]] && pt_pin[i]=$(printf $pt_pin_temp) || pt_pin[i]=$pt_pin_temp
-    done
-}
