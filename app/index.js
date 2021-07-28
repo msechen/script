c6de6e40980d4064f13cb4a1d90c3ad3341d8dd7
drew <68432965+drew0531@users.noreply.github.com>
2021-07-28 20:38:18 +0800

    Update index.js

--- app/index.js
+++ app/index.js
@@ -106,7 +106,6 @@ async function step1() {
         '&returnurl=https://home.m.jd.com/myJd/newhome.action?sceneval=2&ufc=&/myJd/home.action&source=wq_passport',
       'User-Agent':
          randomn(16),
-  *      'jdapp;android;10.0.5;11;0393465333165363-5333430323261368;network/wifi;model/M2102K1C;osVer/30;appBuild/88681;partner/lc001;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; M2102K1C Build/RKQ1.201112.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045534 Mobile Safari/537.36',
       Host: 'plogin.m.jd.com',
     },
   });
@@ -150,7 +149,6 @@ async function step2(cookiesObj) {
         '&returnurl=//home.m.jd.com/myJd/newhome.action?sceneval=2&ufc=&/myJd/home.action&source=wq_passport',
       'User-Agent':
          randomn(16),
-*        'jdapp;android;10.0.5;11;0393465333165363-5333430323261368;network/wifi;model/M2102K1C;osVer/30;appBuild/88681;partner/lc001;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; M2102K1C Build/RKQ1.201112.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045534 Mobile Safari/537.36',
       Host: 'plogin.m.jd.com',
     },
   });
@@ -192,7 +190,6 @@ async function checkLogin(user) {
       Accept: 'application/json, text/plain, */*',
       'User-Agent':
          randomn(16),
-*        'jdapp;android;10.0.5;11;0393465333165363-5333430323261368;network/wifi;model/M2102K1C;osVer/30;appBuild/88681;partner/lc001;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; M2102K1C Build/RKQ1.201112.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045534 Mobile Safari/537.36',
     },
   });
   return response;
