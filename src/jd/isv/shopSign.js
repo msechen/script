d1c56820f607e8263158a703bab4d2195c97e431
jyhuang <326127285@qq.com>
2021-08-02 10:30:01 +0800

    update(jd/signShop): 更新活动

--- src/jd/isv/shopSign.js
+++ src/jd/isv/shopSign.js
@@ -13,13 +13,15 @@ class IsvShopSign extends Isv {
 
     const activityIds = [
       // [activityId, pathId]
-      ['a9a6fe0357f04a6a91328cb35fbd6270', 'sevenDay'],
       ['c2560ac2bcc94894ae41bb66e5921640'],
-      ['92f92e8e6a044e3584ad651622f566ac'],
+      ['5ba8d30ceb224c1e94f9204cc64c5959'],
+      ['267e81b074214ddc873bcd02f7fe906a'],
+      ['2f76681006aa4a7e83845e45c2ff2933'],
+      ['0bd6cdc8ff0e400eb47f4dd1d4543528'],
 
       // 需要加入品牌会员
       // 十月稻田京东自营旗舰店(每月更新)
-      ['736d8aeadd5d463f99a45fec249c205c'],
+      // ['736d8aeadd5d463f99a45fec249c205c'],
     ];
     if (!activityIds.length) return;
     await generateToken(api);
