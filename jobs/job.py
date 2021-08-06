7bb1008bebd9ed7271fc822bfd81be5648461fdf
kollyQAQ <1026774829@qq.com>
2021-08-06 13:34:46 +0800

    u

--- jobs/job.py
+++ jobs/job.py
@@ -115,7 +115,7 @@ def get_jd_order():
     try:
         result = jd.get_order(appkey1, appsecret1)
         if len(result) > 0:
-            earnings = sync_data.query_today_earnings()
+            earnings = sync_data.query_today_earnings() + '\n\n' + sync_data.query_today_data()
             user_kolly.send('[大号]' + result + '\n\n' + earnings)
 
         result = jd.get_order(appkey2, appsecret2)
