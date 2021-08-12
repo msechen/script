875e7e4e7895aa2bc91e35662a6fca66b1eafe45
OrzLee <lijianmin520@gmail.com>
2021-08-13 02:31:50 +0800

    Update crontab_list.sh

--- crontab_list.sh
+++ crontab_list.sh
@@ -20,7 +20,7 @@
 7 0,17 * * * cd /scripts && ts-node /scripts/jd_bean_sign.ts >> /scripts/logs/jd_bean_sign_ts.log 2>&1
 
 # 京东双签
-7 0,17 * * * cd /scripts && ts-node /scripts/jd_bean_box.ts >> /scripts/logs/jd_bean_box.log 2>&1
+0 9,12 * * * cd /scripts && ts-node /scripts/jd_bean_box.ts >> /scripts/logs/jd_bean_box.log 2>&1
 
 # 美丽研究院
 34 9,14,21 * * * node /scripts/jd_beauty.js >> /scripts/logs/jd_beauty.log 2>&1
