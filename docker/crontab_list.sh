77bcde9f28d487227242c9cab24eddd378642c99
ZFeng3242 <pl8354629pou@163.com>
2021-08-21 02:15:33 +0800

    fix

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -123,7 +123,7 @@
 # 京豆变动通知
 0 10 * * * node /scripts/jd_bean_change.js >> /scripts/logs/jd_bean_change.log 2>&1
 # 京东月资产变动通知
-10 7 1-31/7 * * node /scripts/jd_bean_change.js >> /scripts/logs/jd_bean_change.log 2>&1
+10 7 1-31/7 * * node /scripts/jd_bean_MonthChange.js >> /scripts/logs/jd_bean_MonthChange.log 2>&1
 # 京东排行榜
 11 9 * * * node /scripts/jd_rankingList.js >> /scripts/logs/jd_rankingList.log 2>&1
 # 天天提鹅
