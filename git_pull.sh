c84abf00370b7abec53ff36d5818aa0bb973f23f
易享添开 <81273478+1xtk@users.noreply.github.com>
2021-08-25 21:08:21 +0800

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -23,8 +23,8 @@ ContentNewTask=${ShellDir}/new_task
 ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
 isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
-ShellURL=https://hub.fastgit.org/1xtk/jd-Shell
-ScriptsURL=https://hub.fastgit.org/1xtk/JD-get.git
+ShellURL=https://ghproxy.com/https://github.com/1xtk/jd-Shell
+ScriptsURL=https://ghproxy.com/https://github.com/1xtk/JD-get
 
 ## 更新crontab，gitee服务器同一时间限制5个链接，因此每个人更新代码必须错开时间，每次执行git_pull随机生成。
 ## 每天次数随机，更新时间随机，更新秒数随机，至少6次，至多12次，大部分为8-10次，符合正态分布。
