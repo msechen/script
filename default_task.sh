5b5e44a0384ec56cb3981fa7959fb6c6ce4dd43f
Lijianmin <lijianmin520@gmail.com>
2021-09-03 16:30:34 +0800

    fix error

--- default_task.sh
+++ default_task.sh
@@ -253,6 +253,9 @@ crontab $mergedListFile
 echo "第11步将仓库的docker_entrypoint.sh脚本更新至系统/usr/local/bin/docker_entrypoint.sh内..."
 cat /scripts/docker/docker_entrypoint.sh >/usr/local/bin/docker_entrypoint.sh
 
+echo "第12步启动Node HTTP 更新cookie服务进程……"
+pm2 start /scripts/docker/cookie/http.js
+
 echo "发送通知"
 export NOTIFY_CONTENT=""
 node notify_docker_user.js
