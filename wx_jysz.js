166acf7c2167e308a14797cbc73338447e43d1db
msechen <74814336+msechen@users.noreply.github.com>
2021-10-14 17:00:43 +0800

    Add files via upload

--- wx_jysz.js
+++ wx_jysz.js
@@ -1,24 +1,39 @@
-
 /*
+
 软件名称:微信_金银手指
+
 项目注册地址(微信扫码):https://raw.githubusercontent.com/msechen/script/master/photo_2021-09-26_22-41-18.jpg
 
+
 变量抓取:
+
 打开小黄鸟抓包,微信进金银手指界面 找有http://apponlie.sahaj.cn的连接
 点进去他的请求头中token 和 User-Agent
+
 必要变量:
 soy_wx_jysz_token
+
 可选变量
 soy_wx_jysz_User_Agent
+
 多个token用 @ 或 # 或 换行 隔开
+
 v2p配置如下：
+
 【REWRITE】
 匹配链接（正则表达式） http://apponlie.sahaj.cn/user/myInfo
+
 对应重写目标   wx_jysz.js
+
 【MITM】  
 apponlie.sahaj.cn
+
+
 cron 0 8-22/1 * * *
+
 */
+
+
 const $ = new Env('微信_金银手指');
 const notify = $.isNode() ? require('./sendNotify') : '';
 const CryptoJS = require('crypto-js')
@@ -56,7 +71,7 @@ appyq = process.env.appyq;
         };
     });
 
-    soy_wx_jysz_User_Agent = process.env.soy_wx_jysz_User_Agent
+    soy_wx_jysz_User_Agent = process.env.soy_wx_jysz_User_Agent.split()
     
     
 }else{
@@ -64,18 +79,18 @@ appyq = process.env.appyq;
     await get_appdata()
   } else{
   app_soy_wx_jysz_token.push($.getdata('soy_wx_jysz_token'))
-  soy_wx_jysz_User_Agent.push($.getdata('soy_wx_jysz_User_Agent'))
-  
-  }
-apptz = $.getdata('apptz');//是否推送，默认true
-apptx = $.getdata('apptx');//默认提现假,需要自行修改变量
-appyq = $.getdata('appyq');//默认邀请,需要自行修改变量
+  soy_wx_jysz_User_Agent = $.getdata('soy_wx_jysz_User_Agent')
+  apptz = $.getdata('apptz');
+apptx = $.getdata('apptx');
+appyq = $.getdata('appyq');
     
     let jyszcount = ($.getval('jyszcount') || '1');
   for (let i = 2; i <= jyszcount; i++) {
     app_soy_wx_jysz_token.push($.getdata(`soy_wx_jysz_token${i}`))
    
 }
+  }
+
 }
     console.log(
         `\n=== 脚本执行 - 北京时间：${new Date(
@@ -90,15 +105,12 @@ appyq = $.getdata('appyq');//默认邀请,需要自行修改变量
       
 for (i = 0; i < app_soy_wx_jysz_token.length; i++) {
     soy_wx_jysz_token=app_soy_wx_jysz_token[i]
-    //soy_wx_jysz_headers=soy_wx_jysz_User_Agent
-    //JSON.stringify(
+    
     if(!soy_wx_jysz_User_Agent){
         soy_wx_jysz_User_Agent='Mozilla/5.0 (iPhone; CPU iPhone OS 12_5_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.13(0x18000d31) NetType/WIFI Language/zh_CN'
-        soy_wx_jysz_headers= {"Host": "apponlie.sahaj.cn","Accept": "application/json","Origin": "http://ppllmm.zhuwentao52.top","User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_5_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.13(0x18000d31) NetType/WIFI Language/zh_CN","token": `${soy_wx_jysz_token}`,"X-Requested-With": "com.tencent.mm","Referer": "http://ppllmm.zhuwentao52.top/"}
-    }else{
-      soy_wx_jysz_headers= {"Host": "apponlie.sahaj.cn","Accept": "application/json","Origin": "http://ppllmm.zhuwentao52.top","User-Agent": `${soy_wx_jysz_User_Agent}`,"token": `${soy_wx_jysz_token}`,
-      "X-Requested-With": "com.tencent.mm","Referer": "http://ppllmm.zhuwentao52.top/"} 
     }
+    soy_wx_jysz_headers= {"Host": "apponlie.sahaj.cn","Accept": "application/json","Origin": "http://ppllmm.zhuwentao52.top","User-Agent": `${soy_wx_jysz_User_Agent}`,"token": `${soy_wx_jysz_token}`,
+      "X-Requested-With": "com.tencent.mm","Referer": "http://ppllmm.zhuwentao52.top/"} 
     
     
     $.index = i + 1;
@@ -107,7 +119,8 @@ for (i = 0; i < app_soy_wx_jysz_token.length; i++) {
     
         await soy_jysz_Info()
         await soy_jysz_fetchTask()
-        await soy_jysz_TX()
+        await soy_jysz_TX_state()
+        //await soy_jysz_TX()
     
     
 };
@@ -152,10 +165,13 @@ function soy_jysz_Info(){
             let result = JSON.parse(data)
             if(result.code==0){
                 gold=result.data.goldNow
-                txgold=gold/4000*0.35
-                console.log(`\n【${$.name}---用户信息】: \n---用户昵称：${result.data.nameNick}\n---当前剩余金币：${gold}\n---可提现金额：${txgold.toFixed(1)}`)
+                credit = result.data.credit
+                txgold=gold/4000*0.4
+                //console.log(Math.floor(gold/4000*0.4))
+                console.log(`\n【${$.name}---账号 ${$.index} 用户信息】: \n---用户昵称：${result.data.nameNick}\n---用户信用：${credit}\n---当前剩余金币：${gold}\n---可提现金额：${txgold.toFixed(1)}`)
+               
             }else{
-                console.log(`\n【${$.name}---用户信息】: ${result.msg}`)
+                console.log(`\n【${$.name}---账号 ${$.index} 用户信息】: ${result.msg}`)
             }
             
             resolve()
@@ -177,22 +193,23 @@ function soy_jysz_fetchTask() {
                 taskId = result.data.taskId
                 TodayCount = result.data.completeTodayCount
                 TodayGold = result.data.completeTodayGold
-                console.log(`\n【${$.name}---阅读状态】: \n---今日阅读次数:${TodayCount}\n---今日金币：${TodayGold}`)
-                if (TodayCount >= 25) {
+                console.log(`\n【${$.name}---账号 ${$.index} 阅读状态】: \n---今日阅读次数:${TodayCount}\n---今日金币：${TodayGold}`)
+                if (TodayCount == 25) {
                     await soy_jysz_taskSeq(1)
                 }
-                if (TodayCount >= 70) {
+                if (TodayCount == 70) {
                     await soy_jysz_taskSeq(2)
                 }
                 if (taskId == null&&result.data.bizCode==30){
-                    console.log(`\n【${$.name}---阅读状态】:获取任务失败，下批文章将在24小时后到来`)
+                    console.log(`\n【${$.name}---账号 ${$.index} 阅读状态】:下批文章将在24小时后到来,请自行手动过检测在执行`)
                 }
                 if (taskId == null&&result.data.bizCode==11){
-                    console.log(`\n【${$.name}---阅读状态】:获取任务失败，当天达到上限`)
+                    console.log(`\n【${$.name}---账号 ${$.index} 阅读状态】:当天达到上限`)
                 }
                 if (taskId == null&&result.data.bizCode==10){
-                    console.log(`\n【${$.name}---阅读状态】:获取任务失败，下批文章将在60分钟后到达`)
+                    console.log(`\n【${$.name}---账号 ${$.index} 阅读状态】:下批文章将在60分钟后到达`)
                 }
+                
                 if (taskId !== null) {
                     let key = CryptoJS.enc.Utf8.parse("5kosc7jy2w0fxx3s")
                     let plaintText = `{"taskId":${taskId}}`
@@ -204,7 +221,7 @@ function soy_jysz_fetchTask() {
                 
                 
             }else{
-                console.log(`\n【${$.name}---阅读状态】: ${result.msg}`)
+                console.log(`\n【${$.name}---账号 ${$.index} 阅读状态】: ${result.msg}`)
             }
             resolve()
         })
@@ -220,7 +237,7 @@ function soy_jysz_taskSeq(type) {
         }, async(error, response, data) => {
             //console.log(data)
             let result = JSON.parse(data)
-            console.log(`\n【${$.name}---阅读状态】: ${result.msg}`)
+            console.log(`\n【${$.name}---账号 ${$.index} 每日任务】: ${result.msg}`)
 
             resolve()
         })
@@ -239,11 +256,11 @@ function soy_jysz_task(data) {
             //console.log(data)
             let result = JSON.parse(data)
             if(result.code==0){
-                console.log(`\n【${$.name}---阅读第${TodayCount+1}次文章】: 获得 ${result.data.goldAward} 金币`)
+                console.log(`\n【${$.name}---账号 ${$.index} 阅读第${TodayCount+1}次文章】: 获得 ${result.data.goldAward} 金币`)
                 await $.wait(Math.floor(Math.random()*(50000-2000+1000)+2000))
                 await soy_jysz_fetchTask()
             }else{
-              console.log(`\n【${$.name}---阅读第${TodayCount}次文章】: ${result.msg}`)
+              console.log(`\n【${$.name}---账号 ${$.index} 阅读第${TodayCount}次文章】: ${result.msg}`)
             }
             
             resolve()
@@ -251,7 +268,30 @@ function soy_jysz_task(data) {
     })
 }
 
-async function soy_jysz_TX() {
+
+function soy_jysz_TX(body) {
+    return new Promise((resolve, reject) => {
+        $.post({
+            url : `http://apponlie.sahaj.cn/task/completeTask`,
+            headers : {"Accept": "application/json","Content-Type": "application/json;charset=UTF-8","Host": "apponlie.sahaj.cn","Origin": "http://jjuuii.sahaj.cn","Referer": "http://jjuuii.sahaj.cn","token": soy_wx_jysz_token,"User-Agent": soy_wx_jysz_User_Agent,"X-Requested-With": "com.tencent.mm"},
+            body : `${body}`,
+        }, async(error, response, data) => {
+            //console.log(data)
+            let result = JSON.parse(data)
+            if(result.code==0){
+                console.log(`\n【${$.name}---账号 ${$.index} 提现】: 提现成功`)
+            }else{
+              console.log(`\n【${$.name}---账号 ${$.index} 提现】: ${result.msg}`)
+            }
+            
+            resolve()
+        })
+    })
+}
+
+
+
+async function soy_jysz_TX_state() {
     return new Promise((resolve, reject) => {
         $.get({
             url : `http://apponlie.sahaj.cn/user/myInfo`,
@@ -262,40 +302,26 @@ async function soy_jysz_TX() {
             let result = JSON.parse(data)
             if(result.code==0){
                 gold=result.data.goldNow
-                if (gold >= 5000){
-                    txgold=gold/4000*0.35
+                if (gold >= 4000){
+                    if(gold>=1.2/0.4*4000){
+                    txgold=1.2
+                }else if(gold>=0.8/0.4*4000){
+                    txgold=0.8
+                }else{
+                    txgold=0.4
+                }
+                    
                     let key = CryptoJS.enc.Utf8.parse("5kosc7jy2w0fxx3s")
                     let plaintText = `{"moneyPick":${txgold}}`
                     let jm = CryptoJS.AES.encrypt(plaintText, key, {mode: CryptoJS.mode.ECB,padding: CryptoJS.pad.Pkcs7})
-                    return new Promise((resolve, reject) => {
-                        $.post({
-                            url : `http://apponlie.sahaj.cn/user/pickAuto`,
-                            headers : {"Accept": "application/json","Content-Type": "application/json;charset=UTF-8","Host": "apponlie.sahaj.cn","Origin": "http://ppllmm.zhuwentao52.top","Referer": "http://ppllmm.zhuwentao52.top","token": soy_wx_jysz_token,"User-Agent": soy_wx_jysz_User_Agent,"X-Requested-With": "com.tencent.mm"
-                                
-                            },
-                            body : `${jm}`,
-                            
-                        }, async(error, response, data) => {
-                            //console.log(data)
-                            let result = JSON.parse(data)
-                            if (result.code == 0) {
-                                console.log(`\n【${$.name}---提现】: 提现成功`)
-                                
-                            } else {
-                                console.log(`\n【${$.name}---提现】: ${result.msg}`)
-                                
-                            }
-                            resolve()
-                            
-                        })
-                        
-                    })
+                    await soy_jysz_TX(jm)
+                    
                 }else{
-                   console.log(`\n【${$.name}---提现】: 余额不足,无法提现`) 
+                   console.log(`\n【${$.name}---账号 ${$.index} 提现】: 余额不足,无法提现`) 
                 }
                 
             }else{
-                console.log(`\n【${$.name}---提现信息】: ${result.msg}`)
+                console.log(`\n【${$.name}---账号 ${$.index} 提现信息】: ${result.msg}`)
             }
             
             resolve()
