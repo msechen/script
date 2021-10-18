dd34c6cabb278e1da5803f1de275e5be41694237
kuilei0926 <1120391530@qq.com>
2021-10-18 11:58:28 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+助力池（带自动提交）
+
+1.下载jdshare.py  
+服务器运行 wget https://raw.githubusercontent.com/kuilei0926/JDSHARE/main/jdshare.py  
+打开文件修改最后一行，把80改成自己想要的端口  
+
+2.安装依赖  
+pip3 install flask  
+
+3.运行  
+nohup python3 jdshare.py &  
+
+4.青龙拉库  
+ql repo https://ghproxy.com/https://github.com/kuilei0926/JDSHARE.git "" "jdshare.py"  
+
+5.设置环境变量  
+export JDSHAREURL="服务器地址（端口）"  
+例如：  
+export JDSHAREURL="http://127.0.0.1:8000"  
+
+
+可以自己把代码里的的助力池链接写死而不用添加环境变量  
+js里面搜索(process.env.JDSHAREURL+`) 复制括号里面的替换成 (`http://127.0.0.1:8000)
---
+++ jdshare.py
@@ -0,0 +1,55 @@
+from flask import Flask
+from flask import request
+import json,random,sqlite3
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    print('Hello World!')
+    return 'Hello World!'
+
+@app.route('/api/<value>/<int:num>')
+def api(value,num):
+    conn = sqlite3.connect('jdcode.db')
+    c = conn.cursor()
+    cursor = c.execute("SELECT * from "+value)
+    results = cursor.fetchall()
+    try:
+        code = random.sample(results, num)
+        sharecode=[]
+        for i in code:
+            sharecode.append(i[0])
+        conn.close()
+        return json.dumps({"code": 200,"data": sharecode})
+    except:
+        return json.dumps({"code": 500,"data": "互助码数量不足！"})
+@app.route('/api/runTimes')
+def getsharecode():
+    activityId = request.args.get("activityId")
+    sharecode = request.args.get("sharecode")
+    conn = sqlite3.connect('jdcode.db')
+    c = conn.cursor()
+    c.execute("REPLACE INTO "+activityId+"(code,time) VALUES ('%s' ,date());" % (sharecode))
+    conn.commit()
+    conn.close()
+    return json.dumps({"code":200,"data":"ok"})
+
+if __name__ == '__main__':
+    conn = sqlite3.connect('jdcode.db')
+    try:
+        conn.execute("CREATE TABLE farm(code TEXT PRIMARY KEY NOT NULL,time TIME NOT NULL);")
+        conn.execute("CREATE TABLE bean(code TEXT PRIMARY KEY NOT NULL,time TIME NOT NULL);")
+        conn.execute("CREATE TABLE pet(code TEXT PRIMARY KEY NOT NULL,time TIME NOT NULL);")
+        conn.execute("CREATE TABLE sgmh(code TEXT PRIMARY KEY NOT NULL,time TIME NOT NULL);")
+        conn.execute("CREATE TABLE cfd(code TEXT PRIMARY KEY NOT NULL,time TIME NOT NULL);")
+        conn.execute("CREATE TABLE health(code TEXT PRIMARY KEY NOT NULL,time TIME NOT NULL);")
+        conn.execute("CREATE TABLE jxdfactory(code TEXT PRIMARY KEY NOT NULL,time TIME NOT NULL);")
+        conn.execute("CREATE TABLE ddfactory(code TEXT PRIMARY KEY NOT NULL,time TIME NOT NULL);")
+    except:
+        print ("Create table failed")
+    conn.commit()
+    conn.close()
+    app.run(host='0.0.0.0',port=80)
+    
