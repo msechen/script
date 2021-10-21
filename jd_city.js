25c96de38bb185da82921b23bc6a07e27168196e
ZFeng3242 <pl8354629pou@163.com>
2021-10-21 16:26:17 +0800

    更新

--- jd_city.js
+++ jd_city.js
@@ -338,7 +338,7 @@ function shareCodesFormat() {
     $.newShareCodes = [];
     if ($.shareCodesArr[$.index - 1]) {
       $.newShareCodes = $.shareCodesArr[$.index - 1].split('@');
-    } else {
+    } else if(inviteCodes){
       console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
       const tempIndex = $.index > inviteCodes.length ? (inviteCodes.length - 1) : ($.index - 1);
       $.newShareCodes = inviteCodes[tempIndex].split('@');
