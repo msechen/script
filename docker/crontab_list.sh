0d732ea49a9a326fd001f222cf34aedacd4f20bc
ZFeng3242 <pl8354629pou@163.com>
2021-10-23 23:25:40 +0800

    新增推一推，内置助力，介意勿用！

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -93,6 +93,8 @@
 0 0,12,18,21 * * * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
 # 手机狂欢城助力
 10 0,8 * * * node /scripts/jd_carnivalcity_help.js >> /scripts/logs/jd_carnivalcity_help.log 2>&1
+# 推一推
+0 6-23/4 * * * node /scripts/jd_tyt.js >> /scripts/logs/jd_tyt.log 2>&1
 
 ##############长期活动##############
 # 签到
