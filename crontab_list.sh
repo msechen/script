7ce3359d2e46abff6f208b20188a718950caa349
OrzLee <lijianmin520@gmail.com>
2021-10-26 23:08:08 +0800

    Update crontab_list.sh

--- crontab_list.sh
+++ crontab_list.sh
@@ -1,6 +1,9 @@
 # 每3天的23:50分清理一次日志(互助码不清理，proc_file.sh对该文件进行了去重)
 50 23 */3 * * find /scripts/logs -name '*.log' | grep -v 'sharecodeCollection' | xargs rm -rf
 
+# wskey 更新cookie
+20 8,13,20 * * *  cd /scripts/docker/cookie && node gen_open_token.js >> /scripts/logs/gen_open_token.log 2>&1
+
 # 收集助力码
 30 * * * * sh +x /scripts/docker/auto_help.sh collect >> /scripts/logs/auto_help_collect.log 2>&1
 
