369a6a91fc5ed48efcc4d68cea48d8d705eb8204
jyhuang <326127285@qq.com>
2021-10-28 16:33:56 +0800

    update(app): 新增 Combination 脚本

---
+++ src/jd/combination/encrypt.js
@@ -0,0 +1,145 @@
+const CryptoJS = require('crypto-js');
+const {getMoment} = require('../../lib/moment');
+
+var _0xod9 = 'jsjiami.com.v6',
+  _0x5bef = [_0xod9, 'anFxVlM=', 'Q0FmRE8=', 'ekV0aUk=', 'cHVzaA==', 'cHVqUkM=', 'bGVuZ3Ro', 'am9pbg==', 'QkVuS2U=', 'TEJlUFk=', 'c29ydA==', 'bWQ1', 'bGxOa3A=', 'ZnVuY3Rpb24=', 'c3ltYm9s', 'SHVOTkg=', 'cWRrS3o=', 'Q1l3eFQ=', 'Y2hPTlM=', 'aXRlcmF0b3I=', 'bXdqYk8=', 'VlBpVVY=', 'Y29uc3RydWN0b3I=', 'WVZUZVE=', 'cHJvdG90eXBl', 'RWdlUVc=', 'MDcwMzVjYWJiODRsbTY5NA==', 'Z2V0VGltZQ==', 'SHdLdHg=', 'ZXN5Ulo=', 'RnlPSHE=', 'c3RyaW5n', 'b2JqZWN0', 'WkJvb1c=', 'c3BsaXQ=', 'aVJYUlg=', 'ZnpVclE=', 'VnhmVEc=', 'Vm9LTGk=', 'QU5kTWo=', 'aG9kaWg=', 'ZENOQWM=', 'VGpXQnE=', 'Q05nYmE=', 'JjzusjHMGQiarhmHGJQiH.cXom.v6=='];
+(function (_0x4ab3e5, _0x5222de, _0x31f1e0) {
+  var _0x404a60 = function (_0x567d24, _0x32f6e1, _0x4d9fdd, _0x359358, _0x5a5227) {
+    _0x32f6e1 = _0x32f6e1 >> 0x8, _0x5a5227 = 'po';
+    var _0xfb4b7a = 'shift', _0x3c8756 = 'push';
+    if (_0x32f6e1 < _0x567d24) {
+      while (--_0x567d24) {
+        _0x359358 = _0x4ab3e5[_0xfb4b7a]();
+        if (_0x32f6e1 === _0x567d24) {
+          _0x32f6e1 = _0x359358;
+          _0x4d9fdd = _0x4ab3e5[_0x5a5227 + 'p']();
+        } else if (_0x32f6e1 && _0x4d9fdd['replace'](/[JzuHMGQrhHGJQHX=]/g, '') === _0x32f6e1) {_0x4ab3e5[_0x3c8756](_0x359358);}
+      }
+      _0x4ab3e5[_0x3c8756](_0x4ab3e5[_0xfb4b7a]());
+    }
+    return 0xa5c5b;
+  };
+  return _0x404a60(++_0x5222de, _0x31f1e0) >> _0x5222de ^ _0x31f1e0;
+}(_0x5bef, 0x1cc, 0x1cc00));
+var _0x3a21 = function (_0x26c357, _0x31e3a6) {
+  _0x26c357 = ~~'0x'['concat'](_0x26c357);
+  var _0x38f6ed = _0x5bef[_0x26c357];
+  if (_0x3a21['ZjnQwh'] === undefined) {
+    (function () {
+      var _0x55d77d = function () {
+        var _0x2d6df5;
+        try {_0x2d6df5 = Function('return\x20(function()\x20' + '{}.constructor(\x22return\x20this\x22)(\x20)' + ');')();} catch (_0x1c1a2a) {_0x2d6df5 = window;}
+        return _0x2d6df5;
+      };
+      var _0x4bec7d = _0x55d77d();
+      var _0x5d2b3f = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+      _0x4bec7d['atob'] || (_0x4bec7d['atob'] = function (_0x15447c) {
+        var _0x27cd06 = String(_0x15447c)['replace'](/=+$/, '');
+        for (var _0x52d59e = 0x0, _0x492075, _0x408e69, _0x44b376 = 0x0, _0x2e4eb8 = ''; _0x408e69 = _0x27cd06['charAt'](_0x44b376++); ~_0x408e69 && (_0x492075 = _0x52d59e % 0x4 ? _0x492075 * 0x40 + _0x408e69 : _0x408e69, _0x52d59e++ % 0x4) ? _0x2e4eb8 += String['fromCharCode'](0xff & _0x492075 >> (-0x2 * _0x52d59e & 0x6)) : 0x0) {_0x408e69 = _0x5d2b3f['indexOf'](_0x408e69);}
+        return _0x2e4eb8;
+      });
+    }());
+    _0x3a21['oGvQST'] = function (_0x4882b7) {
+      var _0x487293 = atob(_0x4882b7);
+      var _0x258ead = [];
+      for (var _0x4f2fa0 = 0x0, _0xa2ac1d = _0x487293['length']; _0x4f2fa0 < _0xa2ac1d; _0x4f2fa0++) {_0x258ead += '%' + ('00' + _0x487293['charCodeAt'](_0x4f2fa0)['toString'](0x10))['slice'](-0x2);}
+      return decodeURIComponent(_0x258ead);
+    };
+    _0x3a21['Ajteia'] = {};
+    _0x3a21['ZjnQwh'] = !![];
+  }
+  var _0xc96c33 = _0x3a21['Ajteia'][_0x26c357];
+  if (_0xc96c33 === undefined) {
+    _0x38f6ed = _0x3a21['oGvQST'](_0x38f6ed);
+    _0x3a21['Ajteia'][_0x26c357] = _0x38f6ed;
+  } else {_0x38f6ed = _0xc96c33;}
+  return _0x38f6ed;
+};
+
+function M(_0xc44a8f, _0x429a90, _0xdee559) {
+  var _0x4488a6 = {
+    'CNgba': function (_0x215ac0, _0x215aad) {return _0x215ac0 == _0x215aad;},
+    'fzUrQ': _0x3a21('0'),
+    'jqqVS': function (_0x287be6, _0x58e96c) {return _0x287be6 + _0x58e96c;},
+    'CAfDO': function (_0x38b00c, _0x1906ca) {return _0x38b00c == _0x1906ca;},
+    'ANdMj': _0x3a21('1'),
+    'zEtiI': function (_0xba739d, _0x149c91) {return _0xba739d(_0x149c91);},
+    'pujRC': function (_0x5325b4, _0x2366cf) {return _0x5325b4 + _0x2366cf;},
+    'VxfTG': function (_0x5173a1, _0x5eb552) {return _0x5173a1 + _0x5eb552;},
+    'iRXRX': function (_0x411582, _0x32e553) {return _0x411582 == _0x32e553;},
+    'VoKLi': function (_0x1e1ab6, _0xc55723) {return _0x1e1ab6 == _0xc55723;},
+    'hodih': function (_0xa982a7, _0x531bdc) {return _0xa982a7(_0x531bdc);},
+    'dCNAc': function (_0x17dede, _0x2a2f62) {return _0x17dede !== _0x2a2f62;},
+    'TjWBq': _0x3a21('2'),
+    'BEnKe': function (_0x390681, _0x2994f8) {return _0x390681 + _0x2994f8;},
+    'LBePY': function (_0x74c611, _0x49f5f4) {return _0x74c611 + _0x49f5f4;},
+    'llNkp': function (_0x302fe3, _0x5de6de) {return _0x302fe3 + _0x5de6de;},
+  };
+  var _0x1f1f5c = '', _0x110178 = _0xdee559[_0x3a21('3')]('?')[0x1] || '';
+  if (_0xc44a8f) {
+    if (_0x4488a6[_0x3a21('4')](_0x4488a6[_0x3a21('5')], typeof _0xc44a8f)) _0x1f1f5c = _0x4488a6[_0x3a21('6')](_0xc44a8f, _0x110178); else if (_0x4488a6[_0x3a21('7')](_0x4488a6[_0x3a21('8')], _0x4488a6[_0x3a21('9')](x, _0xc44a8f))) {
+      if (_0x4488a6[_0x3a21('a')](_0x4488a6[_0x3a21('b')], _0x4488a6[_0x3a21('b')])) {
+        if (_0x4488a6[_0x3a21('c')](_0x4488a6[_0x3a21('5')], typeof _0xc44a8f)) _0x1f1f5c = _0x4488a6[_0x3a21('d')](_0xc44a8f, _0x110178); else if (_0x4488a6[_0x3a21('e')](_0x4488a6[_0x3a21('8')], _0x4488a6[_0x3a21('f')](x, _0xc44a8f))) {
+          var _0x3ff3f9 = [];
+          for (var _0x3a1019 in _0xc44a8f) _0x3ff3f9[_0x3a21('10')](_0x4488a6[_0x3a21('d')](_0x4488a6[_0x3a21('11')](_0x3a1019, '='), _0xc44a8f[_0x3a1019]));
+          _0x1f1f5c = _0x3ff3f9[_0x3a21('12')] ? _0x4488a6[_0x3a21('6')](_0x3ff3f9[_0x3a21('13')]('&'), _0x110178) : _0x110178;
+        }
+      } else {
+        var _0x407463 = [];
+        for (var _0x3f044c in _0xc44a8f) _0x407463[_0x3a21('10')](_0x4488a6[_0x3a21('14')](_0x4488a6[_0x3a21('15')](_0x3f044c, '='), _0xc44a8f[_0x3f044c]));
+        _0x1f1f5c = _0x407463[_0x3a21('12')] ? _0x4488a6[_0x3a21('15')](_0x407463[_0x3a21('13')]('&'), _0x110178) : _0x110178;
+      }
+    }
+  } else _0x1f1f5c = _0x110178;
+  if (_0x1f1f5c) {
+    var _0xed2a35 = _0x1f1f5c[_0x3a21('3')]('&')[_0x3a21('16')]()[_0x3a21('13')]('');
+    return CryptoJS.MD5(_0x4488a6[_0x3a21('18')](_0xed2a35, _0x429a90));
+    return _0x4488a6[_0x3a21('18')](_0xed2a35, _0x429a90);
+  }
+  return CryptoJS.MD5(_0x429a90);
+  return _0x429a90;
+}
+
+function x(_0x543ed7) {
+  var _0x59f6a8 = {
+    'mwjbO': function (_0x1d95c8, _0x185b22) {return _0x1d95c8 === _0x185b22;},
+    'qdkKz': _0x3a21('19'),
+    'VPiUV': function (_0x44e857, _0x30eba3) {return _0x44e857 === _0x30eba3;},
+    'YVTeQ': function (_0x14d902, _0x3e6382) {return _0x14d902 !== _0x3e6382;},
+    'chONS': _0x3a21('1a'),
+    'HuNNH': function (_0x49e6ac, _0x3b178a) {return _0x49e6ac === _0x3b178a;},
+    'CYwxT': function (_0x3acb6a, _0x86b540) {return _0x3acb6a === _0x86b540;},
+    'EgeQW': function (_0x3eb366, _0x504ccf) {return _0x3eb366(_0x504ccf);},
+  };
+  return x = _0x59f6a8[_0x3a21('1b')](_0x59f6a8[_0x3a21('1c')], typeof Symbol) && _0x59f6a8[_0x3a21('1d')](_0x59f6a8[_0x3a21('1e')], typeof Symbol[_0x3a21('1f')]) ? function (_0x543ed7) {return typeof _0x543ed7;} : function (_0x543ed7) {return _0x543ed7 && _0x59f6a8[_0x3a21('20')](_0x59f6a8[_0x3a21('1c')], typeof Symbol) && _0x59f6a8[_0x3a21('21')](_0x543ed7[_0x3a21('22')], Symbol) && _0x59f6a8[_0x3a21('23')](_0x543ed7, Symbol[_0x3a21('24')]) ? _0x59f6a8[_0x3a21('1e')] : typeof _0x543ed7;}, _0x59f6a8[_0x3a21('25')](x, _0x543ed7);
+}
+
+// '/tzh/combination/indexInfo', {'activityId': 12}
+function getSign(url, form) {
+  var _0x574204 = {
+    'HwKtx': _0x3a21('26'),
+    'esyRZ': function (_0x3b0b1b, _0x1b2926) {return _0x3b0b1b + _0x1b2926;},
+    'FyOHq': function (_0x37ac02, _0x313227, _0xc09a5c, _0x49ff18) {return _0x37ac02(_0x313227, _0xc09a5c, _0x49ff18);},
+  };
+  // timestamp
+  // let _0x3df4f3 = new Date()[_0x3a21('27')]();
+  let _0x3df4f3 = getMoment().valueOf();
+  form['t'] = _0x3df4f3;
+  var _0x25b268 = _0x574204[_0x3a21('28')];
+  var _0x3c4a45 = _0x3df4f3, _0x553e06 = _0x574204[_0x3a21('29')](_0x25b268, _0x3c4a45);
+  let _0x70c008 = {'sign': _0x574204[_0x3a21('2a')](M, form, _0x553e06, url).toString(), 'timestamp': _0x3c4a45};
+  return _0x70c008;
+};_0xod9 = 'jsjiami.com.v6';
+
+module.exports = {
+  getSign,
+};
+
+
+// test
+
+// console.log(getSign('/tzh/combination/doTask', {
+//   activityId: 14,
+//   id: 202110001,
+//   taskId: 3,
+// }))
---
+++ src/jd/combination/index.js
@@ -0,0 +1,97 @@
+const Template = require('../base/template');
+
+const {sleep, writeFileJSON, singleRun} = require('../../lib/common');
+const _ = require('lodash');
+const {getSign} = require('./encrypt');
+const {replaceObjectMethod} = require('../../lib/common');
+
+const indexUrl = 'https://prodev.m.jd.com/mall/active/2VyRHGE7jM1igBJcrjoB6ak1JJWV/index.html';
+const activityId = 14;
+
+class Combination extends Template {
+  static scriptName = 'Combination';
+  static scriptNameDesc = '电脑配件';
+  static dirname = __dirname;
+  static shareCodeTaskList = [];
+  static commonParamFn = () => ({});
+  static times = 1;
+  static needInAppComplete = true;
+
+  static apiOptions = {
+    options: {
+      uri: 'https://combination.m.jd.com/tzh/combination',
+      qs: {
+        activityId,
+      },
+      headers: {
+        origin: 'https://pro.m.jd.com',
+        referrer: 'https://pro.m.jd.com',
+      },
+    },
+  };
+
+  static isSuccess(data) {
+    return _.property('code')(data) === 200;
+  };
+
+  static async doMain(api, shareCodes) {
+    const self = this;
+
+    replaceApi();
+
+    for (let i = 0; i < 2; i++) {
+      await handleDoTask();
+    }
+
+    function replaceApi() {
+      replaceObjectMethod(api, 'doPath', ([functionId, form, options]) => {
+        const {sign, timestamp} = getSign(`/tzh/combination/${functionId}`, {activityId, ...form});
+        options = _.merge({}, options, {
+          headers: {
+            sign, timestamp,
+          },
+          form: {
+            t: timestamp,
+          },
+        });
+        return [functionId, form, options];
+      });
+    }
+
+    async function handleDoTask() {
+      const {list: allTask, extraTaskStatus} = await api.doPath('indexInfo').then(_.property('data')) || {};
+
+      for (const {taskId, taskList} of allTask) {
+        for (let {id, status} of taskList) {
+          status = +status;
+          if (status === 4) continue;
+          const browseTask = status === 2;
+          const onlyGetPrize = status === 3;
+          !onlyGetPrize && await api.doPath('doTask', {id, taskId}).then(logBeanNumber);
+          if (onlyGetPrize || browseTask) {
+            browseTask && await sleep(6);
+            await api.doPath('getPrize', {id, taskId}).then(logBeanNumber);
+          }
+          await sleep(2);
+        }
+      }
+
+      if (extraTaskStatus === '3') {
+        await api.doPath('extraTaskPrize').then(logBeanNumber);
+      }
+    }
+
+    function logBeanNumber(data) {
+      if (!self.isSuccess(data)) {
+        api.log(data.msg);
+        throw new Error('豆已抢光, 停止脚本');
+      }
+      const number = _.property('data.jdNum')(data) || 0;
+      number && api.log(`获得豆豆: ${number}`);
+    }
+  }
+}
+
+singleRun(Combination).then();
+
+module.exports = Combination;
