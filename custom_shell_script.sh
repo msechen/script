9520ea6ceefd3cbdd5112c8a407380d487c6f87f
Lijianmin <lijianmin520@gmail.com>
2021-11-09 23:35:35 +0800

    update

--- custom_shell_script.sh
+++ custom_shell_script.sh
@@ -2,7 +2,7 @@ customListFile="/scripts/logs/other_scripts/Annyoo2021/docker/crontab_list.sh"
 mergedListFile="/scripts/docker/merged_list_file.sh"
 
 if [ -f "$customListFile" ]; then
-  git reset --hard
+  git -C /scripts/logs/other_scripts/Annyoo2021 reset --hard
   git -C /scripts/logs/other_scripts/Annyoo2021 pull --rebase
 else
   git clone https://github.com/Annyoo2021/scripts.git /scripts/logs/other_scripts/Annyoo2021
