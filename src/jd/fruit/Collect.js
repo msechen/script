6f3b6462711739d0d4fc0e659afe11dfbf8ff824
jyhuang <326127285@qq.com>
2021-11-26 16:30:00 +0800

    perf(jd/FruitCollect): 增加兑换逻辑和输出

--- src/jd/fruit/Collect.js
+++ src/jd/fruit/Collect.js
@@ -22,7 +22,14 @@ class FruitCollect extends Template {
   static async doMain(api, shareCodes) {
     const self = this;
 
-    const {result: {popWindow, taskInfo}} = await api.doFormBody('collect_Init', {'channel': 1});
+    const {result: {popWindow, taskInfo, activityStatus}} = await collectInit();
+    const finishedStatus = 4;
+    if (activityStatus === finishedStatus) {
+      return api.log(`已完成本次集勋章活动`);
+    } else if (activityStatus === 3) {
+      return getAwardInfo();
+    }
+
     if (popWindow['windowType'] === 2) {
       // 获取新手奖励
       await api.doFormBody('collect_newUserAward').then(data => api.log(data.msg));
@@ -34,6 +41,34 @@ class FruitCollect extends Template {
           api.log(`完成 ${medalName}, 获得水滴: ${_.get(data, 'result.awardValue', 0)}`);
         });
       }
+    }
+
+    await logMedalInfo();
+
+    async function collectInit() {
+      return api.doFormBody('collect_Init', {'channel': 1});
+    }
+
+    async function logMedalInfo() {
+      const getNames = array => _.map(array, 'medalName').join(',');
+      const {result: {medalInfo}} = await collectInit();
+      const finishedMedals = medalInfo.filter(({status}) => status === finishedStatus);
+      const notFinishedMedals = medalInfo.filter(({status}) => status !== finishedStatus);
+      const msgs = [];
+      if (!_.isEmpty(finishedMedals)) {
+        msgs.push(`已完成的勋章: ${getNames(finishedMedals)}`);
+      }
+      if (!_.isEmpty(notFinishedMedals)) {
+        msgs.push(`未完成的勋章: ${getNames(notFinishedMedals)}`);
+      }
+      api.log(msgs.join(', '));
+    }
+
+    async function getAwardInfo() {
+      const {chooseAwardSubTitle, awardList} = await api.doFormBody('getAwardInfo').then(_.property('result'));
+      return api.log(chooseAwardSubTitle || '勋章已集全, 建议早点领取');
+      // 默认领取水滴, 按需进行
+      await api.doFormBody('collect_exchangeAward', {type: 1});
     }
   }
 }
