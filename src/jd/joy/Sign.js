e8c1b91fd203558b3c090c2f860cf5fe478574d8
jyhuang <326127285@qq.com>
2021-12-07 17:25:55 +0800

    update(jd/JoySign): 更新活动
    - 优化输出

--- src/jd/joy/Sign.js
+++ src/jd/joy/Sign.js
@@ -22,39 +22,47 @@ class JoySign extends Joy {
     const signConfig = [];
 
     const actIds = [
-      {name: 'plus会员店', actId: 'N9MpLQdxZgiczZaMx2SzmSfZSvF'},
+      // {name: 'plus会员店', actId: 'N9MpLQdxZgiczZaMx2SzmSfZSvF'},
       // {name: '小家电', actId: 'c46tGzwvXueH7uKSjpXmPQP9Nod'},
       // {name: '京东失眠-补贴', actId: 'UcyW9Znv3xeyixW1gofhW2DAoz4'},
-      {name: '京东手机-小时', actId: '4Vh5ybVr98nfJgros5GwvXbmTUpg'},
+      // {name: '京东手机-小时', actId: '4Vh5ybVr98nfJgros5GwvXbmTUpg'},
       // {name: '京东商城-卡包', actId: '7e5fRnma6RBATV9wNrGXJwihzcD'},
       // {name: '京东商城-内衣', actId: '4PgpL1xqPSW1sVXCJ3xopDbB1f69'},
       {name: '京东商城-电竞', actId: 'CHdHQhA5AYDXXQN9FLt3QUAPRsB'},
       // {name: '京东商城-箱包', actId: 'ZrH7gGAcEkY2gH8wXqyAPoQgk6t'},
       {name: '京东商城-服饰', actId: '4RBT3H9jmgYg1k2kBnHF8NAHm7m8'},
       {name: '京东商城-校园', actId: '2QUxWHx5BSCNtnBDjtt5gZTq7zdZ'},
-      {name: '京东商城-健康', actId: 'w2oeK5yLdHqHvwef7SMMy4PL8LF'},
+      // {name: '京东商城-健康', actId: 'w2oeK5yLdHqHvwef7SMMy4PL8LF'},
       // {name: '京东商城-鞋靴', actId: '4RXyb1W4Y986LJW8ToqMK14BdTD'},
       // {name: '京东商城-童装', actId: '3Af6mZNcf5m795T8dtDVfDwWVNhJ'},
       // {name: '京东商城-母婴', actId: '3BbAVGQPDd6vTyHYjmAutXrKAos6'},
-      {name: '京东商城-数码', actId: '4SWjnZSCTHPYjE5T7j35rxxuMTb6'},
+      // {name: '京东商城-数码', actId: '4SWjnZSCTHPYjE5T7j35rxxuMTb6'},
       // {name: '京东商城-女装', actId: 'DpSh7ma8JV7QAxSE2gJNro8Q2h9'},
       {name: '京东商城-图书', actId: '3SC6rw5iBg66qrXPGmZMqFDwcyXi'},
       {name: '京东拍拍-二手', actId: '3S28janPLYmtFxypu37AYAGgivfp'},
       // {name: '京东商城-菜场', actId: 'Wcu2LVCFMkBP3HraRvb7pgSpt64'},
       {name: '京东商城-陪伴', actId: 'kPM3Xedz1PBiGQjY4ZYGmeVvrts'},
       // {name: '京东商城-清洁', actId: '2Tjm6ay1ZbZ3v7UbriTj6kHy9dn6'},
-      {name: '京东商城-个护', actId: '2tZssTgnQsiUqhmg5ooLSHY9XSeN'},
+      // {name: '京东商城-个护', actId: '2tZssTgnQsiUqhmg5ooLSHY9XSeN'},
       // {name: '京东商城-家电', actId: '3uvPyw1pwHARGgndatCXddLNUxHw'},
     ];
 
     await updateTurnTableId();
 
     for (const {name, turnTableId, params} of _.filter(_.uniq(signConfig))) {
+      const log = (isSuccess, msg) => {
+        const msgs = [
+          `[${name}]`,
+          isSuccess ? '签到成功' : '签到失败',
+          msg,
+        ];
+        api.log(msgs.join(' '));
+      };
       if (turnTableId) {
         handleSign(turnTableId, name).then(data => {
           if (!data) return;
-          if (!self.isSuccess(data)) return api.log(`[${name}] 签到失败, ${data['errorMessage']}`);
-          api.log(`[${name}] 签到成功, 获得 ${_.get(data, 'data.jdBeanQuantity', 0)}`);
+          if (!self.isSuccess(data)) return log(false, data['errorMessage']);
+          log(true, `获得 ${_.get(data, 'data.jdBeanQuantity', 0)}`);
         });
       } else if (params) {
         api.commonDo({
@@ -68,7 +76,13 @@ class JoySign extends Joy {
             clientVersion: '1.0.0',
           },
         }).then(data => {
-          api.log(`[${name}] 签到成功, ${JSON.stringify(data)}`);
+          const {subCode, subCodeMsg, awardList, noAwardTxt} = data;
+          if (subCode === '0') {
+            let awardText = _.map(awardList || [], 'text').join(', ');
+            log(true, awardText || noAwardTxt);
+          } else {
+            log(false, subCodeMsg);
+          }
         });
       }
     }
