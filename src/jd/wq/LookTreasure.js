adae5e276713ac639508f00e1ea4f9714d5b5517
jyhuang <326127285@qq.com>
2021-12-13 16:15:59 +0800

    perf(jd/LookTreasure): 增加逻辑

--- src/jd/wq/LookTreasure.js
+++ src/jd/wq/LookTreasure.js
@@ -22,12 +22,12 @@ class LookTreasure extends WqBase {
 
   static async beforeRequest(api) {
     replaceObjectMethod(api, 'doGetPath', ([functionId, qs, options]) => {
-      qs = qs || {};
-      _.assign(qs, {
+      qs = {
         _: getMoment().valueOf(),
         g_login_type: 0,
         callback: functionId.split('_')[0],
-      });
+        ...qs,
+      };
       return [functionId, qs, options];
     });
   }
@@ -38,14 +38,35 @@ class LookTreasure extends WqBase {
     const loginSuccess = await self.mLoginWeb(api, indexUrl);
     if (!loginSuccess) return;
     await self.beforeRequest(api);
-    const {first, ownprize: ownPrize, prize} = await api.doGetPath('query_copytemporary');
-    // TODO 通过比较 ownPrize 和 prize
-    if (first === 0) {
-      await api.doGetPath('draw_copytemporary').then(data => {
-        if (!self.isSuccess(data)) return api.log('抽取失败');
-        const {sPrizeDesc, sPrizeName} = data['prize'];
-        api.log(`抽到${sPrizeDesc}: ${sPrizeName}`);
-      });
+
+    await handleDrawFans();
+    await handleDrawVideo();
+
+    // https://wqs.jd.com/sns/202009/22/fansactivecopy/index.html
+    async function handleDrawFans() {
+      const {first, ownprize: ownPrize, prize} = await api.doGetPath('query_copytemporary');
+      // TODO 通过比较 ownPrize 和 prize
+      if (first === 0) {
+        await api.doGetPath('draw_copytemporary').then(data => {
+          if (!self.isSuccess(data)) return api.log('抽取失败');
+          const {sPrizeDesc, sPrizeName} = data['prize'];
+          api.log(`[fans] 抽到${sPrizeDesc}: ${sPrizeName}`);
+        });
+      }
+    }
+
+    // https://wqs.jd.com/sns/202108/03/videofangrowth/index.html
+    async function handleDrawVideo(maxTimes = 5) {
+      if (maxTimes <= 0) return;
+      // TODO 查询, 不确定是否有用
+      await api.doGetPath('query_fans', {callback: 'queryLibao'});
+      const {prize, msg} = await api.doGetPath('draw_fans', {callback: 'openLibao'});
+      if (_.isEmpty(prize)) {
+        api.log(msg);
+        await sleep(2);
+        return handleDrawVideo(--maxTimes);
+      }
+      api.log(`[video] 获得${_.map(prize, 'sPrizeName').join(', ')}`);
     }
   }
 }
