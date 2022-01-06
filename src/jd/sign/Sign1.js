4919b3f2d4be9bbdccf7f1c6865810bbaf0e5a79
jyhuang <326127285@qq.com>
2022-01-06 18:16:21 +0800

    update(jd/Sign1): 增加逻辑

--- src/jd/sign/Sign1.js
+++ src/jd/sign/Sign1.js
@@ -37,11 +37,12 @@ class Sign1 extends Template {
       // 'https://pro.m.jd.com/mall/active/3joSPpr7RgdHMbcuqoRQ8HbcPo9U/index.html',
       'https://pro.m.jd.com/mall/active/ZrH7gGAcEkY2gH8wXqyAPoQgk6t/index.html',
       'https://pro.m.jd.com/mall/active/kPM3Xedz1PBiGQjY4ZYGmeVvrts/index.html',
+      'https://prodev.m.jd.com/mall/active/46xH2FEswob1Eibj3tJMTTfpJvA/index.html',
     ];
 
     for (const url of urlArray) {
       const actId = url.split('/')[url.split('/').length - 2];
-      const encryptProjectId = await getParamFromUrl(url);
+      const encryptProjectId = await getParamFromUrl(url, actId);
       if (!encryptProjectId) continue;
       const assignmentList = await api.doFormBody('queryInteractiveInfo', {encryptProjectId}).then(_.property('assignmentList'));
       for (const {encryptAssignmentId, ext, completionFlag} of assignmentList) {
@@ -71,12 +72,63 @@ class Sign1 extends Template {
       }
     }
 
-    async function getParamFromUrl(url) {
-      return api.doGetFileContent(url, {
+    async function getParamFromUrl(url, actId) {
+      const data = await api.doGetFileContent(url, {
         headers: {
           'user-agent': api.options.headers['user-agent'],
+          cookie: api.cookie,
         },
-      }).then(data => matchMiddle(data, {reg: /\\"encryptProjectId\\":\\"(\w*)\\"/}) || '');
+      });
+      let encryptProjectId = matchMiddle(data, {reg: /\\"encryptProjectId\\":\\"(\w*)\\"/}) || '';
+      if (!encryptProjectId) {
+        const page = matchMiddle(data, {reg: /"paginationFlrs":"(\[\[.+?]])"/});
+        encryptProjectId = _.get(await qryH5BabelFloors(actId, page), 'encryptProjectId');
+      }
+      return encryptProjectId;
+    }
+
+    async function qryH5BabelFloors(activityId, paginationFlrs) {
+      let paginationParam;
+      let paginationParamUpdated = false;
+      return _do(true);
+
+      function _do(isInit = false) {
+        if (isInit) {
+          paginationParam = 1;
+        }
+        if (paginationParam < 0) return;
+        return api.commonDo({
+          ignorePrintLog: true,
+          uri: 'https://api.m.jd.com/client.action',
+          headers: {
+            'user-agent': api.options.headers['user-agent'],
+          },
+          qs: {
+            client: 'wh5',
+            clientVersion: '1.0.0',
+            functionId: 'qryH5BabelFloors',
+          },
+          form: {
+            body: {activityId, paginationParam: `${paginationParam}`, paginationFlrs},
+          },
+        }).then(data => {
+          const target = _.get(data, 'floorList', []).find(o => _.get(o, 'boardParams.interaction', '').match('encryptProjectId'));
+          if (!target) {
+            if (!paginationParamUpdated) {
+              paginationParam = +_.max(data.paginationParams);
+              paginationParamUpdated = true;
+            } else {
+              --paginationParam;
+            }
+            return _do();
+          }
+          let result;
+          try {
+            result = JSON.parse(_.get(target, 'boardParams.interaction'));
+          } catch (e) {}
+          return result;
+        });
+      }
     }
   }
 }
