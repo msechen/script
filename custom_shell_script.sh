ebac1cc60860488f1f41407edbf82c1ffa7c7612
OrzLee <lijianmin520@gmail.com>
2022-01-09 23:59:30 +0800

    Update custom_shell_script.sh

--- custom_shell_script.sh
+++ custom_shell_script.sh
@@ -3,7 +3,7 @@ mergedListFile="/scripts/docker/merged_list_file.sh"
 
 if [ -f "$customListFile" ]; then
   git -C /scripts/logs/other_scripts/Annyoo2021 reset --hard
-  git -C /scripts/logs/other_scripts/Annyoo2021 pull --rebase
+  git -C /scripts/logs/other_scripts/Annyoo2021 pull
 else
   git clone https://github.com/Annyoo2021/scripts.git /scripts/logs/other_scripts/Annyoo2021
 fi
