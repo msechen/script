ccca14726ff0faa6e0e36bc5247b074c5ad8d7a9
ZFeng3242 <pl8354629pou@163.com>
2022-01-09 20:29:30 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -113,6 +113,8 @@
 15 10 * * * node /scripts/jd_jfcz.js >> /scripts/logs/jd_jfcz.log 2>&1
 # 城城分现金
 10 * * * * node /scripts/jd_city.js >> /scripts/logs/jd_city.log 2>&1
+# 京享红包
+0 * * * * node /scripts/jd_redEnvelope.js.js >> /scripts/logs/jd_redEnvelope.js.log 2>&1
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
