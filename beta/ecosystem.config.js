87f87504856ac8f9f33d8f84e0e7e3a786ca6646
Chiupam <65508083+chiupam@users.noreply.github.com>
2022-01-13 20:16:44 +0800

    监控间隔1500毫秒

--- beta/ecosystem.config.js
+++ beta/ecosystem.config.js
@@ -8,15 +8,15 @@ module.exports = {
         autorestart: true,
         watch: ["jbot"],
         ignore_watch: [
+            "jbot/*.log",
+            "jbot/*/*.log",
             "jbot/__pycache__/*",
             "jbot/bot/__pycache__/*",
             "jbot/diy/__pycache__/*",
-            "jbot/*.log",
-            "jbot/*/*.log",
             "jbot/requirements.txt",
             "jbot/ecosystem.config.js"
         ],
-        watch_delay: 15000,
+        watch_delay: 1500,
         interpreter: ""
     }]
 }
