6990e4a13e50abd44954014f590703091020ba08
jyhuang <326127285@qq.com>
2022-01-27 10:29:04 +0800

    fix(jd/HealthShare): 修正名字

--- src/jd/wfh/HealthShare.js
+++ src/jd/wfh/HealthShare.js
@@ -3,6 +3,7 @@ const HealthTemplate = require('./Health');
 const {sleep, writeFileJSON} = require('../../lib/common');
 
 class HealthShare extends HealthTemplate {
+  static scriptNameDesc = '健康社区-助力';
   static shareCodeTaskList = [];
   static time = 2;
   static commonParamFn = () => ({"taskId": 6, "channelId": 1});
