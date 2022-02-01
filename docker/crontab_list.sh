3329a9b703ca1e597f841123b3e2a52e8f7a7aae
ZFeng3242 <pl8354629pou@163.com>
2022-02-01 23:22:57 +0800

    移除过期

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -117,8 +117,6 @@
 10 6,10,12 * * * node /scripts/jd_mpdzcar_game.js >> /scripts/logs/jd_mpdzcar_game.log 2>&1
 #头文字J 助力
 10 3 * * * node /scripts/jd_mpdzcar_help.js >> /scripts/logs/jd_mpdzcar_help.log 2>&1
-#京喜领88元红包
-30 0,8,22 * * * node /scripts/jd_jxlhb.js >> /scripts/logs/jd_jxlhb.log 2>&1
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
