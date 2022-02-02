7813bf3f001d7926499c2e12781caa0a69df3d75
orzlee <lijianmin520@gmail.com>
2022-02-03 00:16:53 +0800

    update

--- cookie/gen_open_token.js
+++ cookie/gen_open_token.js
@@ -5,20 +5,29 @@ const fs = require('fs');
 const os = require('os')
 const common = require('./common')
 
-const body = '%7B%22action%22%3A%22to%22%2C%22to%22%3A%22https%253A%252F%252Fplogin.m.jd.com%252Fcgi-bin%252Fm%252Fthirdapp_auth_page%253Ftoken%253DAAEAIEijIw6wxF2s3bNKF0bmGsI8xfw6hkQT6Ui2QVP7z1Xg%2526client_type%253Dandroid%2526appid%253D879%2526appup_type%253D1%22%7D'
-const clientVersion = '10.1.2'
+const body = '%7B%22to%22%3A%22https%253a%252f%252fplogin.m.jd.com%252fjd-mlogin%252fstatic%252fhtml%252fappjmp_blank.html%22%7D&'
+const userAgent = 'jdapp;android;10.2.2;12;4458733482863700-7863352443708840;model/M2102K1C;addressid/6843817282;aid/a43dbe6cccc9f73d;oaid/ad1e58849a3882b7;osVer/31;appBuild/91077;partner/yybd01;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 12; M2102K1C Build/SKQ1.211006.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/94.0.4606.85 Mobile Safari/537.36'
 
-let sign_params, tokenKey, pt_key, pt_pin, error_msg = '', username = ''
+let tokenKey, pt_key, pt_pin, error_msg = '', username = '', signParams;
 !(async () => {
     const wskeys = getWsKeys();
     if (wskeys.length) {
         console.log(wskeys)
-        await getSign();
-        if (!sign_params) {
+        await axios.get(Buffer.from('aHR0cHM6Ly9jZi5zaGl6dWt1Lm1sL2dlblRva2Vu', 'base64').toString(),
+            {
+                maxRedirects: 0,
+                headers: {
+                    'User-Agent': userAgent,
+                }
+            }
+        ).then(res => {
+            signParams = res.data
+        })
+        if (!signParams) {
             await notify.sendNotify('Update cookie', `获取签名参数错误：\n\n${e}`)
             process.exit(1)
         }
-        console.log('签名参数：' + JSON.stringify(sign_params))
+        console.log('签名参数：' + JSON.stringify(signParams))
         for (let i = 0; i < wskeys.length; i++) {
             try {
                 username = `${i + 1} ${wskeys[i].match(/(pin=[^;]*)/)[1]}`
@@ -27,7 +36,7 @@ let sign_params, tokenKey, pt_key, pt_pin, error_msg = '', username = ''
                 if (tokenKey) {
                     await appJmp()
                     if (pt_key.indexOf('fake') > -1) {
-                        error_msg += `账号: ${username} wskey已失效`
+                        error_msg += `账号: ${username} wskey已失效\n`
                         console.log(`账号: ${username} wskey已失效`)
                     } else {
                         common.writeFile(pt_key, pt_pin, common.cookies_file)
@@ -35,7 +44,7 @@ let sign_params, tokenKey, pt_key, pt_pin, error_msg = '', username = ''
                     }
                 }
             } catch (e) {
-                error_msg += `账号: ${username} 更新错误！！`
+                error_msg += `账号: ${username} 更新错误！！\n`
                 console.log(`账号: ${username} 更新错误： ${e}`)
             }
         }
@@ -43,103 +52,74 @@ let sign_params, tokenKey, pt_key, pt_pin, error_msg = '', username = ''
             await notify.sendNotify("appToken", error_msg, '', '你好，世界！')
         }
     }
-})()
-
-
-function getWsKeys() {
-    console.log(common.wskeys_file)
-    return fs.readFileSync(common.wskeys_file, 'utf8').split(os.EOL)
-}
-
-function randomString(e) {
-    e = e || 32;
-    var t = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz0123456789",
-        a = t.length,
-        n = "";
-    for (i = 0; i < e; i++) n += t.charAt(Math.floor(Math.random() * a));
-    return n
-}
-
-async function getSign() {
-    if (process.env.JD_WSKEY_SIGN_URL) {
-        await axios.post(process.env.JD_WSKEY_SIGN_URL, {
-            uuid: randomString(16),
-            functionId: "genToken",
-            body: body,
-            version: clientVersion
-        }).then(res => {
-            sign_params = res.data
-        }).catch(e => {
-            console.log(`获取签名参数错误: ${e}`)
-        })
-    } 
-    if (!sign_params || !process.env.JD_WSKEY_SIGN_URL) {
-        await axios.get('https://hellodns.coding.net/p/sign/d/jsign/git/raw/master/sign').then(res => {
-            sign_params = res.data
-        }).catch(e => {
-            console.log(`获取签名参数错误: ${e}`)
-        })
     }
-}
+)
+    ()
 
-async function appJmp() {
-    let param = {
-        'tokenKey': tokenKey,
-        'to': 'https://plogin.m.jd.com/cgi-bin/m/thirdapp_auth_page?token=AAEAIEijIw6wxF2s3bNKF0bmGsI8xfw6hkQT6Ui2QVP7z1Xg',
-        'client_type': 'android',
-        'appid': 879,
-        'appup_type': 1,
+
+    function getWsKeys() {
+        console.log(common.wskeys_file)
+        return fs.readFileSync(common.wskeys_file, 'utf8').split(os.EOL)
     }
-    let url = `https://un.m.jd.com/cgi-bin/app/appjmp?${querystring.stringify(param)}`
-    await axios.get(url,
-        {
-            maxRedirects: 0,
-            headers: {
-                'User-Agent': 'okhttp/3.12.1;jdmall;android;version/10.1.2;build/89743;screen/1440x3007;os/11;network/wifi;',
-                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-            }
-        })
-        .catch((error, request, response) => {
-            if (error.response.status === 302) {
-                let cookie = JSON.stringify(error.response.headers['set-cookie'])
-                pt_key = cookie.match(/(pt_key=[^;]*)/)[1]
-                pt_pin = cookie.match(/(pt_pin=[^;]*)/)[1]
-            } else {
+
+    function randomString(e) {
+        e = e || 32;
+        var t = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz0123456789",
+            a = t.length,
+            n = "";
+        for (i = 0; i < e; i++) n += t.charAt(Math.floor(Math.random() * a));
+        return n
+    }
+
+    async function appJmp() {
+        let param = {
+            'tokenKey': tokenKey,
+            'to': 'https://plogin.m.jd.com/jd-mlogin/static/html/appjmp_blank.html'
+        }
+        let url = `https://un.m.jd.com/cgi-bin/app/appjmp?${querystring.stringify(param)}`
+        await axios.get(url,
+            {
+                maxRedirects: 0,
+                headers: {
+                    'User-Agent': userAgent,
+                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                    'x-requested-with': 'com.jingdong.app.mall'
+                }
+            })
+            .catch((error, request, response) => {
+                if (error.response.status === 302) {
+                    let cookie = JSON.stringify(error.response.headers['set-cookie'])
+                    pt_key = cookie.match(/(pt_key=[^;]*)/)[1]
+                    pt_pin = cookie.match(/(pt_pin=[^;]*)/)[1]
+                } else {
+                    console.log(error)
+                }
+            })
+    }
+
+    async function genToken(wskey) {
+
+        let url = `https://api.m.jd.com/client.action?${querystring.stringify(signParams)}`
+        let data = `body=${body}&`
+        await axios.post(url, data,
+            {
+                headers: {
+                    'cookie': wskey,
+                    'User-Agent': userAgent,
+                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    'charset': 'UTF-8',
+                    'accept-encoding': 'br,gzip,deflate'
+                }
+            })
+            .then(res => {
+                if (res.data.code === '0') {
+                    tokenKey = res.data.tokenKey
+                } else {
+                    tokenKey = ''
+                    console.log(`Token key 獲取失敗`)
+                }
+            })
+            .catch(error => {
                 console.log(error)
-            }
-        })
-}
-
-async function genToken(wskey) {
-    let param = {
-        'functionId': 'genToken',
-        'clientVersion': clientVersion,
-        'client': 'android',
-        'uuid': sign_params.uuid,
-        'st': sign_params.st,
-        'sign': sign_params.sign,
-        'sv': sign_params.sv
+            })
     }
-    let url = `https://api.m.jd.com/client.action?${querystring.stringify(param)}`
-    let data = `body=${body}&`
-    await axios.post(url, data,
-        {
-            headers: {
-                'cookie': wskey,
-                'User-Agent': 'okhttp/3.12.1;jdmall;android;version/10.1.2;build/89743;screen/1440x3007;os/11;network/wifi;',
-                'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                'charset': 'UTF-8',
-                'accept-encoding': 'br,gzip,deflate'
-            }
-        })
-        .then(res => {
-            if (res.data.code === '0') {
-                tokenKey = res.data.tokenKey
-            } else {
-                tokenKey = ''
-            }
-        })
-        .catch(error => {
-            console.log(error)
-        })
-}
