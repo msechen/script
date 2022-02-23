883458d5fb71694685824810397ef537aaa89f3b
soy <1056211611@qq.com>
2022-02-23 08:34:58 +0000

    电玩世界变量文件

---
+++ dwsj_variable_data.js
@@ -0,0 +1,12 @@
+module.exports ={"dwsj_variable_data":{
+    "reward_video":"",
+    "dwsj_data":[{
+        "user_ID":"29999597",
+        "dwsj_token":"",
+        "dwsj_body":""
+        },{
+            "user_ID":"29999597",
+            "dwsj_token":"",
+            "dwsj_body":""  
+        }
+    ]}}
