318ee45388c3d692582c25785cdda7485c349915
soy <1056211611@qq.com>
2022-02-23 10:59:11 +0000

    电玩世界变量配置文件

--- dwsj_variable_data.js
+++ dwsj_variable_data.js
@@ -1,12 +1,14 @@
-module.exports ={"dwsj_variable_data":{
-    "reward_video":"",
+module.exports ={"code":200,"dwsj_variable_data":{
+    "reward_video":"这个暂时用不到,不要理",
     "dwsj_data":[{
-        "user_ID":"29999597",
-        "dwsj_token":"",
-        "dwsj_body":""
+        "user_ID":"这里填用户ID(推荐码都可以),在我的账户里面查看",
+        "dwsj_token":"抓包任意域名为 sdsj.shandw.com 请求头上的 Authorization 值",
+        "dwsj_body":"抓包任意域名为 sdsj.shandw.com 的请求体(body)内容",
+        "dwsj_UA":"抓包任意域名为 sdsj.shandw.com 请求头上的 User-Agent 值"
         },{
-            "user_ID":"29999597",
-            "dwsj_token":"",
-            "dwsj_body":""  
+            "user_ID":"这里是第二个账号,按照上面填法,还有更多的自行模仿添加",
+            "dwsj_token":"这里是第二个账号,按照上面填法,还有更多的自行模仿添加",
+            "dwsj_body":"这里是第二个账号,按照上面填法,还有更多的自行模仿添加",
+            "dwsj_UA":"这里是第二个账号,按照上面填法,还有更多的自行模仿添加"
         }
     ]}}
