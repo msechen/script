32a3686ffeceeb6f900f5aec4c323ca6ad785490
soy <1056211611@qq.com>
2022-02-28 11:09:52 +0000

    电玩世界变量配置文件

--- dwsj_variable_data.js
+++ dwsj_variable_data.js
@@ -3,40 +3,42 @@
 //注意抓包有可能是 https://sdsj.shandw.com 链接 也有可能是 49.233.246.33:18081 ip带端口形式
 //这是3个账户的模板,没那么多的账号就删掉
 //单账号信息板块 {"user_ID":""*******"UA":""} 多账号用 逗号 连接 ,删板块也要删对应逗号,自己对比着修改
-//修改是请注意引号,不填或错填脚本可能会报错
 
 module.exports = {"code":200,"dwsj_variable_data":{
     "config":[{
         "url":"https://gitee.com/soy-tool/app-script/raw/master/app_dwsj.js",
         "update":false,
-        "notice":false
+        "notice":true
     }],
     "user_data":[{
         "user_ID":"填用户ID(或推荐码)",
         "build":true或false(true表示建造建筑,反之),
         "UPbuild":true或false(true表示升级建筑,反之,
-        "signtime":1~23(签到时间段,填1到23任意一个),
+        "signtime":0~23(签到时间段,填0到23任意一个),
         "accelerate":0,
         "worship":0,
+        "action":0,
         "Authorization":"抓包找带有 https://sdsj.shandw.com 链接 或 49.233.246.33:18081 的请求头(headers)上的 Authorization 值",
         "UA":"抓包找带有 https://sdsj.shandw.com 链接 或 49.233.246.33:18081 的请求头(headers)上的 User-Agent(user-agent) 值 次变量为选填"
     },{
         "user_ID":"",
         "build":false,
         "UPbuild":false,
-        "signtime":0,
+        "signtime":6,
         "accelerate":0,
         "worship":0,
+        "action":0,
         "Authorization":"",
         "UA":""
     },{
         "user_ID":"",
         "build":false,
         "UPbuild":false,
-        "signtime":0,
+        "signtime":6,
         "accelerate":0,
         "worship":0,
-        "Authorization":"",
+        "action":0,
+        "Authorization":"8",
         "UA":""
     }
     
