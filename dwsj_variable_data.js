36cf55525ef6f8e0a8b424b90653771057eeb11e
soy <1056211611@qq.com>
2022-02-28 02:01:41 +0000

    电玩世界变量配置文件

--- dwsj_variable_data.js
+++ dwsj_variable_data.js
@@ -1,7 +1,6 @@
 //别问我为什么抓包网络,那就是没有根证书(就是需要root移动证书到/etc/security/cacerts/目录下)
 //不要问我安卓11能不能抓包,安卓11需要手动安装证书
-//请注意填写变量格式,reward_video这个值是使用 单引号 , dwsj_build、dwsj_UPbuild、dwsj_signtime这三是没有引号,其他的都是 双引号
-//注意抓包是也有可能是 https://sdsj.shandw.com 链接 也有可能是 49.233.246.33:18081 ip带端口形式
+//注意抓包有可能是 https://sdsj.shandw.com 链接 也有可能是 49.233.246.33:18081 ip带端口形式
 //这是3个账户的模板,没那么多的账号就删掉
 //单账号信息板块 {"user_ID":""*******"UA":""} 多账号用 逗号 连接 ,删板块也要删对应逗号,自己对比着修改
 //修改是请注意引号,不填或错填脚本可能会报错
@@ -20,7 +19,6 @@ module.exports = {"code":200,"dwsj_variable_data":{
         "accelerate":0,
         "worship":0,
         "Authorization":"抓包找带有 https://sdsj.shandw.com 链接 或 49.233.246.33:18081 的请求头(headers)上的 Authorization 值",
-        "reward_video":'抓包看广告成功后找 https://api-access.pangolin-sdk-toutiao.com/api/ad/union/sdk/reward_video/reward 这条链接(不一定每次都出现) 的请求体(body)(也称提交数据)',
         "UA":"抓包找带有 https://sdsj.shandw.com 链接 或 49.233.246.33:18081 的请求头(headers)上的 User-Agent(user-agent) 值 次变量为选填"
     },{
         "user_ID":"",
@@ -30,7 +28,6 @@ module.exports = {"code":200,"dwsj_variable_data":{
         "accelerate":0,
         "worship":0,
         "Authorization":"",
-        "reward_video":'',
         "UA":""
     },{
         "user_ID":"",
@@ -40,7 +37,6 @@ module.exports = {"code":200,"dwsj_variable_data":{
         "accelerate":0,
         "worship":0,
         "Authorization":"",
-        "reward_video":'',
         "UA":""
     }
     
