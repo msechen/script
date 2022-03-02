785ac383be8f75d2659cd8531ff22e0492c882de
soy <1056211611@qq.com>
2022-03-02 11:24:07 +0000

    达赏变量配置文件

---
+++ ds_variable_data.js
@@ -0,0 +1,12 @@
+module.exports = {"code":200,"ds_variable_data":{
+    "config":[{
+        "url":"https://gitee.com/soy-tool/app-script/raw/master/soy_ds.js",
+        "update":false,
+        "notice":true
+    }],
+    "user_data":[{
+        "user_ID":"用户ID(在我的界面看)",
+        "token":"抓包 链接包含 https://gw.tokshine.com/app/ 的请求头上的 authorization 值"
+        
+    }
+]}}
