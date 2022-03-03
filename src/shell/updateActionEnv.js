8e0f73898a56a7bd1a4590646eea7a90fa7924ea
jyhuang <326127285@qq.com>
2022-03-03 14:55:16 +0800

    update(shell): 增加 updateActionEnv.js

---
+++ src/shell/updateActionEnv.js
@@ -0,0 +1,86 @@
+/**
+ * @description 本地更新远程仓库的 secrets
+ */
+
+const _ = require('lodash');
+const {updateProcessEnv, getEnv} = require('../lib/env');
+const {Octokit} = require('@octokit/core');
+const {readFileJSON} = require('../lib/common');
+
+async function main() {
+  updateProcessEnv();
+
+  const config = [
+    {
+      owner: 'jinboyigu',
+      repo: 'lazy_script',
+    },
+    {
+      owner: 'jinboyigu',
+      repo: 'run_lazy_script',
+    },
+  ];
+
+  const actionName = 'ACTION_ENV';
+  const auth = getEnv('github_api_token');
+  if (!auth) {
+    console.log('请配置 github_api_token');
+  }
+  const MyOctokit = Octokit.defaults({auth});
+
+  for (const options of config) {
+    await updateProductActionEnv(options);
+  }
+
+  console.log(`${actionName} 更新成功`);
+
+  async function updateProductActionEnv(defaultOption) {
+    const octokit = new MyOctokit();
+    const actionEnv = readFileJSON('../../.env.product.json', __dirname);
+    if (_.isEmpty(actionEnv)) {
+      return console.log('.env.product.json is empty');
+    }
+    return updateActionValue(actionName, JSON.stringify(actionEnv));
+
+    async function updateActionValue(name, value) {
+      const {key, key_id} = await request({route: 'GET /repos/{owner}/{repo}/actions/secrets/public-key'});
+      const encryptedValue = encryptActionValue(value, key);
+      return request({method: 'PUT'}, {
+        key_id,
+        secret_name: name,
+        encrypted_value: encryptedValue,
+      });
+    }
+
+    function request({route, method = 'GET'}, options = {}) {
+      route = route || `${method} /repos/{owner}/{repo}/actions/secrets`;
+      if (options['secret_name']) {
+        route += '/{secret_name}';
+      }
+
+      return octokit.request(route, _.assign(defaultOption, options)).then(result => {
+        console.log(result);
+        return _.property('data')(result);
+      });
+    }
+  }
+
+  function encryptActionValue(value, key) {
+    const sodium = require('tweetsodium');
+
+    // key = 'base64-encoded-public-key';
+    // value = 'plain-text-secret';
+
+    // Convert the message and key to Uint8Array's (Buffer implements that interface)
+    const messageBytes = Buffer.from(value);
+    const keyBytes = Buffer.from(key, 'base64');
+
+    // Encrypt using LibSodium.
+    const encryptedBytes = sodium.seal(messageBytes, keyBytes);
+
+    // Base64 the encrypted secret
+    return Buffer.from(encryptedBytes).toString('base64');
+  }
+}
+
+main();
