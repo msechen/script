98f54b661fc324d4945d3c1306c8f0a39df7289b
LingFeng <lingfeng_0918@163.com>
2022-03-15 12:18:53 +0800

    add -

---
+++ ql_jlhb_log.js
@@ -0,0 +1,15 @@
+let logs = [
+// 规则
+]
+if (process.env.logs) {
+    if (process.env.logs.indexOf('\n') > -1) {
+
+        logs = process.env.logs.split('\n');
+    } else {
+        logs = process.env.logs.split();
+    }
+}
+for (let i = 0; i < logs.length; i++) {
+    const index = (i + 1 === 1) ? '' : (i + 1);
+    exports['logs' + index] = logs[i];
+}
