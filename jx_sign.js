cd08585a41c209e32a09c17c18a3c892bf7985a3
ZFeng3242 <pl8354629pou@163.com>
2022-03-19 16:51:33 +0800

    Fix
    ①修复京喜签到
    ②修复种豆得豆

--- jx_sign.js
+++ jx_sign.js
@@ -37,6 +37,7 @@ let cookiesArr = [], cookie = '', message;
 let UA, UAInfo = {}, isLoginInfo = {};
 $.shareCodes = [];
 $.blackInfo = {}
+$.appId = "0ac98";
 const JX_FIRST_RUNTASK = $.isNode() ? (process.env.JX_FIRST_RUNTASK && process.env.JX_FIRST_RUNTASK === 'xd' ? '5' : '1000') : ($.getdata('JX_FIRST_RUNTASK') && $.getdata('JX_FIRST_RUNTASK') === 'xd' ? '5' : '1000')
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
@@ -52,13 +53,7 @@ if ($.isNode()) {
     $.msg($.name, "【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取", "https://bean.m.jd.com/bean/signIndex.action", { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
     return;
   }
-  let res = await getAuthorShareCode('https://raw.githubusercontent.com/DX3242/updateTeam/master/shareCodes/jxSign.json')
-  if (!res) {
-    $.http.get({url: 'https://purge.jsdelivr.net/gh/DX3242/updateTeam@master/shareCodes/jxSign.json'}).then((resp) => {}).catch((e) => $.log('刷新CDN异常', e));
-    await $.wait(1000)
-    res = await getAuthorShareCode('https://cdn.jsdelivr.net/gh/DX3242/updateTeam@master/shareCodes/jxSign.json')
-  }
-  $.shareCodes = [...(res || []), ...$.shareCodes]
+  await requestAlgo();
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
@@ -78,10 +73,8 @@ if ($.isNode()) {
       if (!isLoginInfo[$.UserName]) continue
       if (JX_FIRST_RUNTASK === '5') {
         $.signhb_source = '5'
-        await requestAlgo();
       } else if (JX_FIRST_RUNTASK === '1000') {
         $.signhb_source = '1000'
-        await requestAlgo();
       }
       await signhb(1)
       await $.wait(500)
@@ -117,23 +110,19 @@ if ($.isNode()) {
         console.log(`开始运行喜豆任务`)
         $.taskName = '喜豆'
         $.signhb_source = '5'
-        await requestAlgo();
         await main()
         console.log(`\n开始运行红包任务`)
         $.taskName = '红包'
         $.signhb_source = '1000'
-        await requestAlgo();
         await main(false)
       } else if (JX_FIRST_RUNTASK === '1000') {
         console.log(`开始运行红包任务`)
         $.taskName = '红包'
         $.signhb_source = '1000'
-        await requestAlgo();
         await main()
         console.log(`\n开始运行喜豆任务`)
         $.taskName = '喜豆'
         $.signhb_source = '5'
-        await requestAlgo();
         await main(false)
       }
     }
@@ -212,10 +201,13 @@ async function main(help = true) {
 
 // 查询信息
 function signhb(type = 1) {
-  let body = '';
-  if ($.signhb_source === '5') body = `type=0&signhb_source=${$.signhb_source}&smp=&ispp=1&tk=`
+  let functionId = 'signhb/query', body = '';
+  if ($.signhb_source === '5') {
+    functionId = 'signhb/query_jxpp'
+    body = `type=0&signhb_source=${$.signhb_source}&smp=&ispp=1&tk=`
+  }
   return new Promise((resolve) => {
-    $.get(taskUrl("signhb/query", body), async (err, resp, data) => {
+    $.get(taskUrl(functionId, body), async (err, resp, data) => {
       try {
         if (err) {
           console.log(JSON.stringify(err));
@@ -296,8 +288,16 @@ function signhb(type = 1) {
 
 // 签到 助力
 function helpSignhb(smp = '') {
+  let functionId, body;
+  if ($.signhb_source === '5') {
+    functionId = 'signhb/query_jxpp'
+    body = `type=1&signhb_source=${$.signhb_source}&smp=&ispp=1&tk=`
+  } else {
+    functionId = 'signhb/query'
+    body = `type=1&signhb_source=${$.signhb_source}&smp=${smp}&ispp=0&tk=`
+  }
   return new Promise((resolve) => {
-    $.get(taskUrl("signhb/query", `type=1&signhb_source=${$.signhb_source}&smp=${smp}&ispp=1&tk=`), async (err, resp, data) => {
+    $.get(taskUrl(functionId, body), async (err, resp, data) => {
       try {
         if (err) {
           console.log(JSON.stringify(err))
@@ -330,14 +330,16 @@ function helpSignhb(smp = '') {
 
 // 任务
 function dotask(task) {
-  let body;
+  let functionId, body;
   if ($.signhb_source === '5') {
+    functionId = 'signhb/dotask_jxpp'
     body = `task=${task}&signhb_source=${$.signhb_source}&ispp=1&sqactive=${$.sqactive}&tk=`
   } else {
-    body = `task=${task}&signhb_source=${$.signhb_source}&ispp=1&tk=`
+    functionId = 'signhb/dotask'
+    body = `task=${task}&signhb_source=${$.signhb_source}&ispp=0&sqactive=&tk=`
   }
   return new Promise((resolve) => {
-    $.get(taskUrl("signhb/dotask", body), async (err, resp, data) => {
+    $.get(taskUrl(functionId, body), async (err, resp, data) => {
         try {
           if (err) {
             console.log(JSON.stringify(err));
@@ -368,7 +370,7 @@ function bxdraw() {
   if ($.signhb_source === '5') {
     body = `ispp=1&sqactive=${$.sqactive}&tk=`
   } else {
-    body = `ispp=1&tk=`
+    body = `ispp=0&sqactive=&tk=`
   }
   return new Promise((resolve) => {
     $.get(taskUrl("signhb/bxdraw", body), async (err, resp, data) => {
@@ -539,39 +541,6 @@ function TotalBean() {
     })
   })
 }
-function getAuthorShareCode(url) {
-  return new Promise(resolve => {
-    const options = {
-      url: `${url}?${new Date()}`, "timeout": 10000, headers: {
-        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
-      }
-    };
-    if ($.isNode() && process.env.TG_PROXY_HOST && process.env.TG_PROXY_PORT) {
-      const tunnel = require("tunnel");
-      const agent = {
-        https: tunnel.httpsOverHttp({
-          proxy: {
-            host: process.env.TG_PROXY_HOST,
-            port: process.env.TG_PROXY_PORT * 1
-          }
-        })
-      }
-      Object.assign(options, { agent })
-    }
-    $.get(options, async (err, resp, data) => {
-      try {
-        if (err) {
-        } else {
-          if (data) data = JSON.parse(data)
-        }
-      } catch (e) {
-        // $.logErr(e, resp)
-      } finally {
-        resolve(data);
-      }
-    })
-  })
-}
 function jsonParse(str) {
   if (typeof str == "string") {
     try {
@@ -611,11 +580,6 @@ Date.prototype.Format = function (fmt) {
 }
 
 async function requestAlgo() {
-  if ($.signhb_source === '5') {
-    $.appId = 10038;
-  } else {
-    $.appId = 10028;
-  }
   $.fingerprint = await generateFp();
   const options = {
     "url": `https://cactus.jd.com/request_algo?g_ty=ajax`,
@@ -634,7 +598,7 @@ async function requestAlgo() {
       'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,en;q=0.7'
     },
     'body': JSON.stringify({
-      "version": "1.0",
+      "version": "3.0",
       "fp": $.fingerprint,
       "appId": $.appId.toString(),
       "timestamp": Date.now(),
@@ -697,7 +661,7 @@ function decrypt(time, stk, type, url) {
     const hash2 = $.CryptoJS.HmacSHA256(st, hash1.toString()).toString($.CryptoJS.enc.Hex);
     // console.log(`\nst:${st}`)
     // console.log(`h5st:${["".concat(timestamp.toString()), "".concat(fingerprint.toString()), "".concat($.appId.toString()), "".concat(token), "".concat(hash2)].join(";")}\n`)
-    return encodeURIComponent(["".concat(timestamp.toString()), "".concat($.fingerprint.toString()), "".concat($.appId.toString()), "".concat($.token), "".concat(hash2)].join(";"))
+    return encodeURIComponent(["".concat(timestamp.toString()), "".concat($.fingerprint.toString()), "".concat($.appId.toString()), "".concat($.token), "".concat(hash2), "".concat("3.0"), "".concat(time)].join(";"))
   } else {
     return '20210318144213808;8277529360925161;10001;tk01w952a1b73a8nU0luMGtBanZTHCgj0KFVwDa4n5pJ95T/5bxO/m54p4MtgVEwKNev1u/BUjrpWAUMZPW0Kz2RWP8v;86054c036fe3bf0991bd9a9da1a8d44dd130c6508602215e50bb1e385326779d'
   }
