03d013661acd8706d888fd5f6feb1e3deb2dddca
soy <1056211611@qq.com>
2022-03-22 15:35:37 +0000

    源火星球配置文件

--- yhxq_variable.js
+++ yhxq_variable.js
@@ -9,18 +9,21 @@ module.exports = {"code":200,"yhxq_variable_data":{
         "Withdrawal":(是否提现,true代表提现,false反之),
         "Withdrawal_Time":(提现的时间,请配合自己设定的cron填写)),
         "Feed":(喂养,true代表喂养,false反之),
+        "Hoe":(拔萝卜次数,每天可免费一次,默认填1),
         "UA":"请求UA头,请求头的User-Agent值(选填)"
     },{
         "token":"多号如上,没有就删除,否则报错",
         "Withdrawal":多号如上,没有就删除,否则报错,
         "Withdrawal_Time":多号如上,没有就删除,否则报错,
         "Feed":多号如上,没有就删除,否则报错,
+        "Hoe":(拔萝卜次数,每天可免费一次,默认填1),
         "UA":"多号如上,没有就删除,否则报错"
     },{
         "token":"多号如上,没有就删除,否则报错",
         "Withdrawal":多号如上,没有就删除,否则报错,
         "Withdrawal_Time":多号如上,没有就删除,否则报错,
         "Feed":多号如上,没有就删除,否则报错,
+        "Hoe":(拔萝卜次数,每天可免费一次,默认填1),
         "UA":"多号如上,没有就删除,否则报错"
     }
     
