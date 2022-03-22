9082d0b129cef3b79c0914e941d91fb66d41ef9e
soy <1056211611@qq.com>
2022-03-22 15:32:06 +0000

    源火星球配置文件

--- yhxq_variable.js
+++ yhxq_variable.js
@@ -2,14 +2,26 @@ module.exports = {"code":200,"yhxq_variable_data":{
     "config":[{
         "url":"https://gitee.com/soy-tool/app-script/raw/master/app_yhxq.js",
         "update":false,
-        "notice":false(推送通知,true代表推送,false反之)
+        "notice":false
     }],
     "user_data":[{
         "token":"请求头上的Authorization值",
-        "Withdrawal":false,
-        "Withdrawal_Time":8,
-        "Feed":false(喂养,true代表喂养,false反之),
+        "Withdrawal":(是否提现,true代表提现,false反之),
+        "Withdrawal_Time":(提现的时间,请配合自己设定的cron填写)),
+        "Feed":(喂养,true代表喂养,false反之),
         "UA":"请求UA头,请求头的User-Agent值(选填)"
+    },{
+        "token":"多号如上,没有就删除,否则报错",
+        "Withdrawal":多号如上,没有就删除,否则报错,
+        "Withdrawal_Time":多号如上,没有就删除,否则报错,
+        "Feed":多号如上,没有就删除,否则报错,
+        "UA":"多号如上,没有就删除,否则报错"
+    },{
+        "token":"多号如上,没有就删除,否则报错",
+        "Withdrawal":多号如上,没有就删除,否则报错,
+        "Withdrawal_Time":多号如上,没有就删除,否则报错,
+        "Feed":多号如上,没有就删除,否则报错,
+        "UA":"多号如上,没有就删除,否则报错"
     }
     
 ]}}
