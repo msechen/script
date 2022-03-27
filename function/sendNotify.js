e55e04b32fa4492b8bfe1a3fe464c353c2fccb1d
LingFeng <lingfeng_0918@163.com>
2022-03-27 23:30:46 +0800

    add-检查并停用免密支付 update-适配新版青龙

--- function/sendNotify.js
+++ function/sendNotify.js
@@ -31,7 +31,7 @@ const querystring = require('querystring');
 const exec = require('child_process').exec;
 const $ = new Env();
 const timeout = 15000; //超时时间(单位毫秒)
-console.log("加载sendNotify，当前版本: 20220302");
+console.log("加载sendNotify，当前版本: 20220327");
 // =======================================go-cqhttp通知设置区域===========================================
 //gobot_url 填写请求地址http://127.0.0.1/send_private_msg
 //gobot_token 填写在go-cqhttp文件设置的访问密钥
@@ -149,7 +149,18 @@ const {
     getEnvByPtPin
 } = require('./ql');
 const fs = require('fs');
-let strCKFile = '/ql/scripts/CKName_cache.json';
+let isnewql = fs.existsSync('/ql/data/config/auth.json');
+let strCKFile="";
+let strUidFile ="";
+if(isnewql){
+    strCKFile = '/ql/data/scripts/CKName_cache.json';
+    strUidFile = '/ql/data/scripts/CK_WxPusherUid.json';
+}else{
+    strCKFile = '/ql/scripts/CKName_cache.json';
+    strUidFile = '/ql/scripts/CK_WxPusherUid.json';
+}
+
+
 let Fileexists = fs.existsSync(strCKFile);
 let TempCK = [];
 if (Fileexists) {
@@ -160,7 +171,7 @@ if (Fileexists) {
         TempCK = JSON.parse(TempCK);
     }
 }
-let strUidFile = '/ql/scripts/CK_WxPusherUid.json';
+
 let UidFileexists = fs.existsSync(strUidFile);
 let TempCKUid = [];
 if (UidFileexists) {
@@ -1455,11 +1466,13 @@ async function sendNotify(text, desp, params = {}, author = '\n\n本通知 By ht
                         try {
                             //额外处理1，nickName包含星号
                             $.nickName = $.nickName.replace(new RegExp(`[*]`, 'gm'), "[*]");
-
                             text = text.replace(new RegExp(`${$.UserName}|${$.nickName}`, 'gm'), $.Remark);
-
                             if (text == "京东资产变动" || text == "京东资产变动#2" || text == "京东资产变动#3" || text == "京东资产变动#4") {
-                                var Tempinfo = getQLinfo(cookie, envs[i].created, envs[i].timestamp, envs[i].remarks);
+                                var Tempinfo = "";
+                                if(envs[i].created)
+                                    Tempinfo=getQLinfo(cookie, envs[i].created, envs[i].timestamp, envs[i].remarks);
+                                else
+                                    Tempinfo=getQLinfo(cookie, envs[i].createdAt, envs[i].timestamp, envs[i].remarks);
                                 if (Tempinfo) {
                                     $.Remark += Tempinfo;
                                 }
@@ -1713,7 +1726,12 @@ async function sendNotifybyWxPucher(text, desp, PtPin, author = '\n\n本通知 B
                             //额外处理1，nickName包含星号
                             $.nickName = $.nickName.replace(new RegExp(`[*]`, 'gm'), "[*]");
 
-                            var Tempinfo = getQLinfo(cookie, tempEnv.created, tempEnv.timestamp, tempEnv.remarks);
+                            var Tempinfo = "";
+                            if(tempEnv.created)
+                                Tempinfo=getQLinfo(cookie, tempEnv.created, tempEnv.timestamp, tempEnv.remarks);
+                            else
+                                Tempinfo=getQLinfo(cookie, tempEnv.createdAt, tempEnv.timestamp, tempEnv.remarks);
+
                             if (Tempinfo) {
                                 Tempinfo = $.nickName + Tempinfo;
                                 desp = desp.replace(new RegExp(`${$.UserName}|${$.nickName}`, 'gm'), Tempinfo);
@@ -1989,57 +2007,58 @@ function BarkNotify(text, desp, params = {}) {
 }
 
 function tgBotNotify(text, desp) {
-    return new Promise((resolve) => {
+    return new Promise(resolve => {
         if (TG_BOT_TOKEN && TG_USER_ID) {
             const options = {
                 url: `https://${TG_API_HOST}/bot${TG_BOT_TOKEN}/sendMessage`,
-                body: `chat_id=${TG_USER_ID}&text=${text}\n\n${desp}&disable_web_page_preview=true`,
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
+                json: {
+                    chat_id: `${TG_USER_ID}`,
+                    text: `${text}\n\n${desp}`,
+                    disable_web_page_preview:true,
                 },
-                timeout,
-            };
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                timeout
+            }
             if (TG_PROXY_HOST && TG_PROXY_PORT) {
-                const tunnel = require('tunnel');
+                const tunnel = require("tunnel");
                 const agent = {
                     https: tunnel.httpsOverHttp({
                         proxy: {
                             host: TG_PROXY_HOST,
                             port: TG_PROXY_PORT * 1,
-                            proxyAuth: TG_PROXY_AUTH,
-                        },
-                    }),
-                };
-                Object.assign(options, {
-                    agent
-                });
+                            proxyAuth: TG_PROXY_AUTH
+                        }
+                    })
+                }
+                Object.assign(options, {agent})
             }
             $.post(options, (err, resp, data) => {
                 try {
                     if (err) {
-                        console.log('telegram发送通知消息失败！！\n');
+                        console.log('telegram发送通知消息失败！！\n')
                         console.log(err);
                     } else {
                         data = JSON.parse(data);
                         if (data.ok) {
-                            console.log('Telegram发送通知消息成功🎉。\n');
+                            console.log('Telegram发送通知消息成功�。\n')
                         } else if (data.error_code === 400) {
-                            console.log('请主动给bot发送一条消息并检查接收用户ID是否正确。\n');
+                            console.log('请主动给bot发送一条消息并检查接收用户ID是否正确。\n')
                         } else if (data.error_code === 401) {
-                            console.log('Telegram bot token 填写错误。\n');
+                            console.log('Telegram bot token 填写错误。\n')
                         }
                     }
                 } catch (e) {
                     $.logErr(e, resp);
-                }
-                finally {
+                } finally {
                     resolve(data);
                 }
-            });
+            })
         } else {
-            resolve();
+            resolve()
         }
-    });
+    })
 }
 
 function ddBotNotify(text, desp) {
@@ -2185,7 +2204,7 @@ function ChangeUserId(desp) {
     }
 }
 
-function qywxamNotify(text, desp, strsummary="") {
+function qywxamNotify(text, desp, strsummary = "") {
     return new Promise((resolve) => {
         if (QYWX_AM) {
             const QYWX_AM_AY = QYWX_AM.split(',');
@@ -2203,7 +2222,7 @@ function qywxamNotify(text, desp, strsummary="") {
             $.post(options_accesstoken, (err, resp, data) => {
                 html = desp.replace(/\n/g, '<br/>');
                 html = `<font size="3">${html}</font>`;
-                if (strsummary=="") {
+                if (strsummary == "") {
                     strsummary = desp;
                 }
                 var json = JSON.parse(data);
@@ -2448,7 +2467,7 @@ function wxpusherNotifyByOne(text, desp, strsummary = "") {
             }
 
             if (strsummary.length > 96) {
-                strsummary = strsummary.substring(0, 95)+"...";
+                strsummary = strsummary.substring(0, 95) + "...";
             }
             let uids = [];
             for (let i of WP_UIDS_ONE.split(";")) {
