e1c6afc015b56069aadd0227b564fa2630e44f7d
soy <1056211611@qq.com>
2022-03-29 06:55:59 +0000

    闪电世界配置文件

--- dwsj_variable_data.js
+++ dwsj_variable_data.js
@@ -7,28 +7,28 @@
 module.exports = {"code":200,"dwsj_variable_data":{
     "config":[{
         "url":"https://gitee.com/soy-tool/app-script/raw/master/app_dwsj.js",
-        "update":{true或false,true表示自动更新脚本,反之),
-        "notice":{true或falset,rue表示发送通知,反之),
+        "update":,//true或false,true表示自动更新脚本,反之
+        "notice":,//true或falset,rue表示发送通知,反之
     }],
     "user_data":[{
-        "user_ID":"填用户ID(或推荐码)",
-        "build":(true或false,true表示建造建筑,反之),
-        "UPbuild":(true或false,true表示升级建筑,反之),
-        "signtime":(签到时间段,填0到23任意一个),
-        "transfer":(false或true,true表示转增给第一个账号,反之),
-        "Withdrawal":(false或true,true表示提现,反之.注:只提现平台收益里面的金额,按最高金额开始提现),
-        "delayed":(延时操作,单位秒,填数字不能为负数,如填1则是1秒到2秒之间延时,表示领完收益后延时几秒再丢骰子),
-        "continued":(false或true,true表示持续丢骰子,反之),
-        "fire":(false或true,true表示着火情况下进行灭火,反之),
-        "buy_fire":(false或true,true表示购买消防车,反之),
-        "buy_dice":(false或true,true表示购自定义骰子,反之),
-        "dice_six":(false或true,true表示在着火情况下,身前6格不着火是否丢6电骰子,反之),
-        "Address":"转增地址(对方的)",
-        "gold_coins":"填5/10/20(对应转5m/10m/20m)",
-        "config":(false或true,true表示在没有次数情况(如加速,膜拜)就不会去跑该任务,反之),
-        "reward_video":'观看视频的请求体,链接为:https://api-access.pangolin-sdk-toutiao.com/api/ad/union/sdk/reward_video/reward/',
-        "Authorization":"抓包找带有 https://sdsj.shandw.com 链接 或 49.233.246.33:18081 的请求头(headers)上的 Authorization 值",
-        "UA":"抓包找带有 https://sdsj.shandw.com 链接 或 49.233.246.33:18081 的请求头(headers)上的 User-Agent(user-agent) 值 次变量为选填"
+        "user_ID":"",//填用户ID或推荐码
+        "build":,//true或false,true表示建造建筑,反之
+        "UPbuild":,//true或false,true表示升级建筑,反之
+        "signtime":,//签到时间段,填0到23任意一个
+        "transfer":,//false或true,true表示转增给第一个账号,反之
+        "Withdrawal":,//false或true,true表示提现,反之.注:只提现平台收益里面的金额,按最高金额开始提现
+        "delayed":,//延时操作,单位秒,填数字不能为负数,如填1则是1秒到2秒之间延时,表示领完收益后延时几秒再丢骰子
+        "continued":,//false或true,true表示持续丢骰子,反之
+        "fire":,//false或true,true表示着火情况下进行灭火,反之
+        "buy_fire":,//false或true,true表示购买消防车,反之
+        "buy_dice":,//false或true,true表示购自定义骰子,反之
+        "dice_six":,//false或true,true表示在着火情况下,身前6格不着火是否丢6电骰子,反之
+        "Address":"",//转增地址(对方的)
+        "gold_coins":"",//填5/10/20(对应转5m/10m/20m)
+        "config":,//false或true,true表示在没有次数情况(如加速,膜拜)就不会去跑该任务,反之
+        "reward_video":'',//观看视频的请求体,一般再链接为:https://api-access.pangolin-sdk-toutiao.com/api/ad/union/sdk/reward_video/reward/,找不到就留空
+        "Authorization":"",//抓包找带有 https://sdsj.shandw.com 链接 或 49.233.246.33:18081 的请求头(headers)上的 Authorization 值
+        "UA":""//抓包找带有 https://sdsj.shandw.com 链接 或 49.233.246.33:18081 的请求头(headers)上的 User-Agent(user-agent) 值 次变量为选填
     },{
         "user_ID":"",
         "build":false,
