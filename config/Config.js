488738ec2e04b1d94c99a2151b13d68ca750e8e4
soy <1056211611@qq.com>
2022-04-07 09:01:18 +0000

    update config/Config.js.

--- config/Config.js
+++ config/Config.js
@@ -1,1 +1,7 @@
-{"Script_data":{"sdsj":{"state":1,"content":""},"cs":{"state":0,"content":"测试"}}}
+{"Script_data":[{
+    "sdsj":{"state":1,"content":""
+    }},{
+    "cs":{"state":0,"content":"测试"
+    }}
+
+]}
