bc85c901d384a8fe6b9b9ad99c8501d9ea6f5d81
soy <1056211611@qq.com>
2022-04-29 16:52:11 +0000

    奖券世界配置变量文件

--- soy_jqsj_data.js
+++ soy_jqsj_data.js
@@ -9,8 +9,8 @@ module.exports = {
         "ANDROIDID":"",//同上,找不到随便输入16位
         "oaid":"",//同上,找不到随便输入16位数字和小写字母组合
         "uid":"",//同上,找不到就在链接找或者游戏中我的界面自己的ID
-        "token":"",//同上
-        "User-Agent":"com.caike.ticket/2.3.1 (Linux; U; Android 10; zh-cn) (official; 20301)",//勿动
+        "token":"",//同上,注意极速版token不通用
+        "User-Agent":"com.caike.ticket/2.7.0 (Linux; U; Android 10; zh-cn) (yyb; 20700)",//勿动
         //
         },/*{//第二个账号配置,如此类推,需要把/星 星/删除
         "DEVICEID":"",//抓包带有 https://android-api.lucklyworld.com 链接的请求头找
@@ -18,14 +18,14 @@ module.exports = {
         "oaid":"",//同上
         "uid":"",//同上
         "token":"",//同上
-        "User-Agent":"com.caike.ticket/2.3.1 (Linux; U; Android 9; zh-cn) (official; 20301)",
+        "User-Agent":"com.caike.ticket/2.7.0 (Linux; U; Android 10; zh-cn) (yyb; 20700)",
         },{
         "DEVICEID":"",//抓包带有 https://android-api.lucklyworld.com 链接的请求头找
         "ANDROIDID":"",//同上
         "oaid":"",//同上
         "uid":"",//同上
         "token":"",//同上
-        "User-Agent":"com.caike.ticket/2.3.1 (Linux; U; Android 9; zh-cn) (official; 20301)",
+        "User-Agent":"com.caike.ticket/2.7.0 (Linux; U; Android 10; zh-cn) (yyb; 20700)",
         }*/
 
 ]}
