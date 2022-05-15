455af29ae919c3214267f61d84aff0823ab69ec3
soy <1056211611@qq.com>
2022-05-15 06:04:44 +0000

    一起养蜂

--- soy_yqyf_data.js
+++ soy_yqyf_data.js
@@ -7,7 +7,8 @@ module.exports = {
         "jwt":"xxxxxxxxxxx",//抓带有 https://api.aiqingyinghang.com 的链接,链接上 jwt 值
         "ID":123456,//用户id,在我的界面,ID
         "rouletted":false,//true为转转盘,false为不转
-        "withdraw":true,//true为提现,false为不提现,有次数情况0.3开始
+        "withdraw":true,//true为提现,false为不提现,有次数情况0.3开始(群红包的)
+        "money":20,//提现金额(20元/50元/100元/200元/500元填对应数字)(这个只针对钱包的,会检测withdraw变量)
         "profit":false,//true为领取下级收益,false为不领取
         "page":1,//红包页数,默认是第一页,如果没有红包或一直抢红包失败可尝试改
         "UA":""//抓包的请求头user-agent值
@@ -15,7 +16,8 @@ module.exports = {
         "jwt":"xxxxxxxxxxx",//抓带有 https://api.aiqingyinghang.com 的链接,链接上 jwt 值
         "ID":123456,//用户id,在我的界面,ID
         "rouletted":false,//true为转转盘,false为不转
-        "withdraw":true,//true为提现,false为不提现,有次数情况0.3开始
+        "withdraw":true,//true为提现,false为不提现,有次数情况0.3开始(群红包的)
+        "money":20,//提现金额(20元/50元/100元/200元/500元填对应数字)(这个只针对钱包的,会检测withdraw变量)
         "profit":false,//true为领取下级收益,false为不领取
         "page":1,//红包页数,默认是第一页,如果没有红包或一直抢红包失败可尝试改
         "UA":""//抓包的请求头user-agent值
@@ -23,7 +25,8 @@ module.exports = {
         "jwt":"xxxxxxxxxxx",//抓带有 https://api.aiqingyinghang.com 的链接,链接上 jwt 值
         "ID":123456,//用户id,在我的界面,ID
         "rouletted":false,//true为转转盘,false为不转
-        "withdraw":true,//true为提现,false为不提现,有次数情况0.3开始
+        "withdraw":true,//true为提现,false为不提现,有次数情况0.3开始(群红包的)
+        "money":20,//提现金额(20元/50元/100元/200元/500元填对应数字)(这个只针对钱包的,会检测withdraw变量)
         "profit":false,//true为领取下级收益,false为不领取
         "page":1,//红包页数,默认是第一页,如果没有红包或一直抢红包失败可尝试改
         "UA":""//抓包的请求头user-agent值
