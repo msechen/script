c49784811d981baf43ff23e7055b6ff835a5ba9b
soy <1056211611@qq.com>
2022-05-15 04:43:32 +0000

    一起养蜂配置文件

--- soy_yqyf_data.js
+++ soy_yqyf_data.js
@@ -8,6 +8,7 @@ module.exports = {
         "ID":123456,//用户id,在我的界面,ID
         "rouletted":false,//true为转转盘,false为不转
         "withdraw":true,//true为提现,false为不提现,有次数情况0.3开始
+        "profit":false,//true为领取下级收益,false为不领取
         "page":1,//红包页数,默认是第一页,如果没有红包或一直抢红包失败可尝试改
         "UA":""//抓包的请求头user-agent值
         },/*{//第二个号
@@ -15,6 +16,7 @@ module.exports = {
         "ID":123456,//用户id,在我的界面,ID
         "rouletted":false,//true为转转盘,false为不转
         "withdraw":true,//true为提现,false为不提现,有次数情况0.3开始
+        "profit":false,//true为领取下级收益,false为不领取
         "page":1,//红包页数,默认是第一页,如果没有红包或一直抢红包失败可尝试改
         "UA":""//抓包的请求头user-agent值
         },{//第三个号
@@ -22,6 +24,7 @@ module.exports = {
         "ID":123456,//用户id,在我的界面,ID
         "rouletted":false,//true为转转盘,false为不转
         "withdraw":true,//true为提现,false为不提现,有次数情况0.3开始
+        "profit":false,//true为领取下级收益,false为不领取
         "page":1,//红包页数,默认是第一页,如果没有红包或一直抢红包失败可尝试改
         "UA":""//抓包的请求头user-agent值
         },*/
