a090073486582dd2cb62111ee071ef9dd9a68fd9
kollyQAQ <1026774829@qq.com>
2022-06-27 22:48:51 +0800

    1

--- wx-robot.py
+++ wx-robot.py
@@ -96,6 +96,10 @@ def forward_to_kolly(msg):
             ret = sync_data.query_brand_order('oppo')
             logger.info(ret)
             return ret
+        elif 'VIVO' in msg.text or 'vivo' in msg.text or 'Vivo' in msg.text:  
+            ret = sync_data.query_brand_order('vivo')
+            logger.info(ret)
+            return ret
     elif msg.sender.name == '内测' or msg.sender.name == '「蓝猫」早起俱乐部':
         logger.info('收到群聊「{}」「{}」的消息：{}'.format(msg.sender.name, msg.member.name, msg.text))
         if '#早起打卡' in msg.text:
