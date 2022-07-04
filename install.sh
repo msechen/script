fc07d62b164bb31e78695834bd31379ecb711c8c
rkygogo <107276912+rkygogo@users.noreply.github.com>
2022-07-04 16:08:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -185,6 +185,9 @@ iptables -t mangle -F >/dev/null 2>&1
 iptables -F >/dev/null 2>&1
 iptables -X >/dev/null 2>&1
 netfilter-persistent save >/dev/null 2>&1
+systemctl stop apache2 >/dev/null 2>&1
+systemctl disable apache2 >/dev/null 2>&1
+lsof -i :80|grep -v "PID"|awk '{print "kill -9",$2}'|sh >/dev/null 2>&1
 if [[ -z $(grep 'DiG 9' /etc/hosts) ]]; then
 v4=$(curl -s4m5 https://ip.gs -k)
 if [ -z $v4 ]; then
@@ -193,8 +196,6 @@ echo -e nameserver 2a01:4f8:c2c:123f::1 > /etc/resolv.conf
 fi
 fi
 }
-systemctl stop apache2 >/dev/null 2>&1
-systemctl disable apache2 >/dev/null 2>&1
 install_x-ui() {
     systemctl stop x-ui
     cd /usr/local/
