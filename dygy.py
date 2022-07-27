02be355c928c41e33d05ba2d0a48111bd3d569d4
xiaoyulr <106761309+xiaoyulr@users.noreply.github.com>
2022-07-27 21:25:50 +0800

    Update dygy.py

--- dygy.py
+++ dygy.py
@@ -29,7 +29,7 @@ def polling_info():
 
 ##七天登录奖励
 def sign():
-    url = "https://minigame.zijieapi.com/ttgame/game_orchard_ecom/sign_in/reward?watch_ad=0&extra_ad_num=0&os_version=10&version_code=200900&device_id=708526086828733&iid=3980686141954494&app_name=aweme&device_platform=android&device_type=Pixel%204&channel=sem_shenma_dy_ls146&aid=1128&ac=wifi&version_name=20.9.0&update_version_code=20909900"
+    url = "https://minigame.zijieapi.com/ttgame/game_orchard_ecom/sign_in/reward?"
     response = requests.get(url=url, headers=headers).json()
     if response["status_code"] == 0:
         day = response['data']["reward_item"]["id"]
@@ -42,7 +42,7 @@ def sign():
 ##选择奖励宝箱
 def choose_box_reward():
     compare = []
-    url = "https://minigame.zijieapi.com/ttgame/game_orchard_ecom/challenge/list?os_version=10&version_code=200900&device_id=708526086828733&iid=3980686141954494&app_name=aweme&device_platform=android&device_type=Pixel%204&channel=sem_shenma_dy_ls146&aid=1128&ac=wifi&version_name=20.9.0&update_version_code=20909900"
+    url = "https://minigame.zijieapi.com/ttgame/game_orchard_ecom/challenge/list?"
     response = requests.get(url=url, headers=headers).json()
     # pprint.pprint(response)
     if response["status_code"] == 0:
@@ -59,7 +59,7 @@ def choose_box_reward():
         reward_water = compare[0][1]
         num = compare[0][2]
         print(f'选择{id}号需要浇水{num}次宝箱的{reward_water}水滴宝箱任务')
-        url = f"https://minigame.zijieapi.com/ttgame/game_orchard_ecom/challenge/choose?task_id={str(id)}&os_version=10&version_code=200900&device_id=708526086828733&iid=3980686141954494&app_name=aweme&device_platform=android&device_type=Pixel%204&channel=sem_shenma_dy_ls146&aid=1128&ac=wifi&version_name=20.9.0&update_version_code=20909900"
+        url = f"https://minigame.zijieapi.com/ttgame/game_orchard_ecom/challenge/choose?task_id={str(id)}"
         response = requests.get(url=url, headers=headers).json()
         if response["status_code"] == 0:
             print("挑战开始")
@@ -68,7 +68,7 @@ def choose_box_reward():
         reward_water = compare[1][1]
         num = compare[1][2]
         print(f'选择{id}号需要浇水{num}次宝箱的{reward_water}水滴宝箱任务')
-        url = f"https://minigame.zijieapi.com/ttgame/game_orchard_ecom/challenge/choose?task_id={str(id)}&os_version=10&version_code=200900&device_id=708526086828733&iid=3980686141954494&app_name=aweme&device_platform=android&device_type=Pixel%204&channel=sem_shenma_dy_ls146&aid=1128&ac=wifi&version_name=20.9.0&update_version_code=20909900"
+        url = f"https://minigame.zijieapi.com/ttgame/game_orchard_ecom/challenge/choose?task_id={str(id)}"
         response = requests.get(url=url, headers=headers).json()
         if response["status_code"] == 0:
             print("挑战开始")
@@ -76,7 +76,7 @@ def choose_box_reward():
 
 ##挑战礼包奖励
 def recive_box_reward():
-    url = "https://minigame.zijieapi.com/ttgame/game_orchard_ecom/challenge/reward?os_version=10&version_code=200900&device_id=708526086828733&iid=3980686141954494&app_name=aweme&device_platform=android&device_type=Pixel%204&channel=sem_shenma_dy_ls146&aid=1128&ac=wifi&version_name=20.9.0&update_version_code=20909900"
+    url = "https://minigame.zijieapi.com/ttgame/game_orchard_ecom/challenge/reward?"
     response = requests.get(url=url, headers=headers).json()
     # print(response)
     if response["status_code"] == 0:
@@ -88,7 +88,7 @@ def recive_box_reward():
 
 ##领取储水池奖励有问题
 def reward_bottle():
-    url = "https://minigame.zijieapi.com/ttgame/game_orchard_ecom/water_bottle/reward?os_version=10&version_code=200900&device_id=708526086828733&iid=3980686141954494&app_name=aweme&device_platform=android&device_type=Pixel%204&channel=sem_shenma_dy_ls146&aid=1128&ac=wifi&version_name=20.9.0&update_version_code=20909900"
+    url = "https://minigame.zijieapi.com/ttgame/game_orchard_ecom/water_bottle/reward?"
     response = requests.get(url=url, headers=headers).json()
     pprint.pprint(response)
     if response["status_code"] == 0:
@@ -102,7 +102,7 @@ def reward_bottle():
 def waterwheel():
     for i in range(20):
         try:
-            url = "https://minigame.zijieapi.com/ttgame/game_orchard_ecom/scene/touch?scene_id=1&os_version=10&version_code=200900&device_id=708526086828733&iid=3980686141954494&app_name=aweme&device_platform=android&device_type=Pixel%204&channel=sem_shenma_dy_ls146&aid=1128&ac=wifi&version_name=20.9.0&update_version_code=20909900"
+            url = "https://minigame.zijieapi.com/ttgame/game_orchard_ecom/scene/touch?scene_id=1"
             response = requests.get(url=url, headers=headers).json()
             # pprint.pprint(response)
             if response["data"]["reward_item"] is None:
@@ -117,7 +117,7 @@ def waterwheel():
 
 ##签到领化肥
 def sign_fertilizer():
-    url = "https://minigame.zijieapi.com/ttgame/game_orchard_ecom/nutrient/sign_in?os_version=10&version_code=200900&device_id=708526086828733&iid=3980686141954494&app_name=aweme&device_platform=android&device_type=Pixel%204&channel=sem_shenma_dy_ls146&aid=1128&ac=wifi&version_name=20.9.0&update_version_code=20909900"
+    url = "https://minigame.zijieapi.com/ttgame/game_orchard_ecom/nutrient/sign_in?"
     response = requests.get(url=url, headers=headers).json()
     if response["status_code"] == 0:
         print("签到成功")
@@ -127,7 +127,7 @@ def sign_fertilizer():
 
 ##水滴日常任务领5次水滴
 def day_water():
-    url = "https://minigame.zijieapi.com/ttgame/game_orchard_ecom/tasks/reward?task_id=1&do_action=0&extra_ad_num=0&version=11&os_version=10&version_code=200900&device_id=708526086828733&iid=3980686141954494&app_name=aweme&device_platform=android&device_type=Pixel%204&channel=sem_shenma_dy_ls146&aid=1128&ac=wifi&version_name=20.9.0&update_version_code=20909900"
+    url = "https://minigame.zijieapi.com/ttgame/game_orchard_ecom/tasks/reward?task_id=1"
     response = requests.get(url=url, headers=headers).json()
     # pprint.pprint(response)
     if response["status_code"] == 0:
@@ -139,7 +139,7 @@ def day_water():
 
 ##三餐礼包
 def eat():
-    url = "https://minigame.zijieapi.com/ttgame/game_orchard_ecom/tasks/reward?task_id=2&do_action=1&extra_ad_num=0&version=11&os_version=10&version_code=200900&device_id=708526086828733&iid=3980686141954494&app_name=aweme&device_platform=android&device_type=Pixel%204&channel=sem_shenma_dy_ls146&aid=1128&ac=wifi&version_name=20.9.0&update_version_code=20909900"
+    url = "https://minigame.zijieapi.com/ttgame/game_orchard_ecom/tasks/reward?"
     response = requests.get(url=url, headers=headers).json()
     if response["status_code"] == 0:
         reward_water = response["data"]["reward_item"]["num"]
@@ -150,7 +150,7 @@ def eat():
 
 ##每日领奖励及浇水10次奖励,每次请求一次
 def water_award():
-    url = 'https://minigame.zijieapi.com/ttgame/game_orchard_ecom/box/open?os_version=10&version_code=200900&device_id=708526086828733&iid=3980686141954494&app_name=aweme&device_platform=android&device_type=Pixel%204&channel=sem_shenma_dy_ls146&aid=1128&ac=wifi&version_name=20.9.0&update_version_code=20909900'
+    url = 'https://minigame.zijieapi.com/ttgame/game_orchard_ecom/box/open?'
     response = requests.get(url=url, headers=headers).json()
     if response["status_code"] == 0:
         reward_water = response["data"]["reward_item"]["num"]
@@ -159,11 +159,17 @@ def water_award():
         print(response['message'])
 
 
+def browse_reward():
+    url = "https://minigame.zijieapi.com/ttgame/game_orchard_ecom/sign_in/reward?watch_ad=0&extra_ad_num=0&os_version=15.4.1&version_code=21.6.0&device_id=3224240103102327&iid=550227824358583&app_name=aweme&device_platform=ipad&device_type=iPad6,3&channel=App%20Store&aid=1128&ac=WIFI&version_name=&update_version_code=&scene=022001&sid=28JooKDNkrgqdDn6llEEou5zBiFG3wiXMgQ6VSGKMcX9EiLDr31qjt4ARuuptdNG8UMx1dysKFLGpCDGTwnaS8QhJq4hYlrMUv5bDjtVeN8Yngd7trz4dvQ752sVDZzotNOl3XApffFJw28KwiRpUCBykUrV"
+    response = requests.get(url=url, headers=headers).json()
+    print(response)
+
+
 def watering(i=1):
     num = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0]
     if (i - 1) / 10 in num:
         water_award()
-    url = f"https://minigame.zijieapi.com/ttgame/game_orchard_ecom/tree/water?version=11&os_version=10&version_code=200900&device_id=708526086828733&iid=3980686141954494&app_name=aweme&device_platform=android&device_type=Pixel%204&channel=sem_shenma_dy_ls146&aid=1128&ac=wifi&version_name=20.9.0&update_version_code=20909900"
+    url = f"https://minigame.zijieapi.com/ttgame/game_orchard_ecom/tree/water?"
     response = requests.get(url=url, headers=headers).json()
     # pprint.pprint(response)
     if response["message"] == "success":
@@ -200,7 +206,7 @@ def judy_fertilizer():
 
 ##完成度
 def target():
-    url = 'https://minigame.zijieapi.com/ttgame/game_orchard_ecom/home_info?version=11&os_version=10&version_code=200900&device_id=708526086828733&iid=3980686141954494&app_name=aweme&device_platform=android&device_type=Pixel%204&channel=sem_shenma_dy_ls146&aid=1128&ac=wifi&version_name=20.9.0&update_version_code=20909900'
+    url = 'https://minigame.zijieapi.com/ttgame/game_orchard_ecom/home_info?'
     response = requests.get(url=url, headers=headers).json()
     if response["status_code"] == 0:
         reward_water = response["data"]["info"]["progress"]
@@ -240,8 +246,10 @@ def push_plus_bot(content, push_token):
 
 
 if __name__ == '__main__':
-    cks = os.environ['dygyCookies'].split('@')
-    push_token = os.environ['push_token']
+    cks = 'Mozilla/5.0 (iPad; CPU OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 BytedanceWebview/d8a21c6 Aweme/21.6.0 Mobile ToutiaoMicroApp/2.58.1.2&msToken=N7fP8ds8RJmGLO-6qInielCWpSI6El-KDBVGHdV8TVVVz2NOXISrlZADCz3BoBKXHFc_xoRcZXr4NQ-VmQ==;d_ticket=f79011b7cc6195273c8539d928116881d7a7a;multi_sids=3729153863976576%3A497d8212f5303f555fb95ec3c8192c94;n_mh=QdKkAlx7T9xoYu9vVe2IYwI_GugWM3xcZn4OFZoMrlY;odin_tt=971054b44c8af4fa1239b2ea90eb30e506b0b493e0f61a77327d910541234cf3618c852f8c5ab1c4ed8af64ddaebd97a756d88fab73a43624c0c0c25ad919fbb6f549bc8efa869b16dfce93e15226ea1;sessionid=497d8212f5303f555fb95ec3c8192c94;sessionid_ss=497d8212f5303f555fb95ec3c8192c94;sid_guard=497d8212f5303f555fb95ec3c8192c94%7C1658741925%7C5184000%7CFri%2C+23-Sep-2022+09%3A38%3A45+GMT;sid_tt=497d8212f5303f555fb95ec3c8192c94;uid_tt=aa67e9b522a2bba2e0987724a4260e68;uid_tt_ss=aa67e9b522a2bba2e0987724a4260e68;passport_csrf_token=c6633b304c947d9e348777a7d881cca4;passport_csrf_token_default=c6633b304c947d9e348777a7d881cca4;install_id=550227824358583;ttreq=1$81e19109038ae526d0fbbd0d3ae77b281756c675;msToken=N7fP8ds8RJmGLO-6qInielCWpSI6El-KDBVGHdV8TVVVz2NOXISrlZADCz3BoBKXHFc_xoRcZXr4NQ-VmQ=='
+    cks = cks.split('@')
+    # cks = os.environ['dygyCookies'].split('@')
+    # push_token = os.environ['push_token']
     for ck in cks:
         ck = ck.split('&')
         headers1 = {
@@ -258,9 +266,10 @@ if __name__ == '__main__':
             time.sleep(random.randint(1, 4))
             print('开始水车任务')
             waterwheel()
-        elif start_time == '08':
+        elif start_time == '07':
             print('开始收集蓄水池任务')
             reward_bottle()
+        elif start_time == '19':
             print('开始领取水滴日常任务奖励')
             day_water()
             time.sleep(random.randint(1, 4))
@@ -287,6 +296,7 @@ if __name__ == '__main__':
             time.sleep(random.randint(1, 4))
             print('开始推送信息')
             message = target()
+
         elif start_time == '12':
             print('开始三餐领取任务')
             eat()
@@ -303,7 +313,7 @@ if __name__ == '__main__':
             time.sleep(random.randint(2, 6))
             print('开始领取水滴日常任务奖励')
             day_water()
-        elif start_time == '19':
+        elif start_time == '32':
             print('开始领取挑战宝箱奖励')
             recive_box_reward()
         elif start_time == '21':
@@ -315,7 +325,7 @@ if __name__ == '__main__':
             watering()
             print('开始推送信息')
             message = target()
-            push_plus_bot(message, push_token)
+            # push_plus_bot(message, push_token)
 
 """
 ff668ac51ae0c803&eyJleHAiOjE2NTQ5MTkwNjEsImtpZCI6MCwiYWxnIjoiSFMyNTYifQ.eyJ1aWQiOiI4ODgyNTIwNiIsIm9wZW5pZCI6Im9ZYlZMeExnZThibWFINFhvSVQyMDdQWlJyV3ciLCJleHAiOjE2NTQ5MTkwNjF9.z7QV2gTs2U4tc9OyX02i6kCpw3H7qo7vfQWGe4Itx-g&eyJleHAiOjE2NjI2OTMyNjEsImFsZyI6IkhTMjU2In0.eyJ1aWQiOiI4ODgyNTIwNiIsIm9wZW5pZCI6Im9ZYlZMeExnZThibWFINFhvSVQyMDdQWlJyV3ciLCJleHAiOjE2NjI2OTMyNjF9.7CESzUCev6obxvP-vJWQw1elM2Jcj2jAlzoN6ORiy40
