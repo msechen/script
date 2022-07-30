986ee3c288f69ef11c492408261347c62302b80e
xiaoyulr <106761309+xiaoyulr@users.noreply.github.com>
2022-07-30 09:17:29 +0800

    Update 小龙传奇.py

--- 小龙传奇.py
+++ 小龙传奇.py
@@ -9,11 +9,11 @@ xlcq = os.environ['xlcqacount']
 
 def main():
     print(f"--------共找到{len(xlcq)}个账号--------")
-    for i in xlcq:
-        print(f"------------正在执行第{xlcq.index(i) + 1}个账号------------")
-        phone = i.split('----')[0]
-        pwd = i.split('----')[1]
-        dl(phone, pwd)
+#     for i in xlcq:
+    print(f"------------正在执行第{xlcq.index(i) + 1}个账号------------")
+    phone = i.split('----')[0]
+    pwd = i.split('----')[1]
+    dl(phone, pwd)
 
 def dl(phone, pwd):
     headers={'Host':'xl.qunx.cc:96','Connection':'keep-alive','Content-Length':'33','User-Agent':'Mozilla/5.0 (Linux; Android 12; 2201122C Build/SKQ1.211006.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/95.0.4638.74 Mobile Safari/537.36 Html5Plus/1.0','Content-Type':'application/x-www-form-urlencoded','Origin':'http://xl.qunx.cc:96','X-Requested-With':'nl.yiyou.dragon','Referer':'http://xl.qunx.cc:96/h5/page/login.html','Accept-Encoding':'gzip, deflate','Accept-Language':'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7','Cookie':'PHPSESSID=g19084jhus9lte9fl41sfa1lrm; wx=; qq=; qqq=; token=; mobile=; userid=; nickname=; is_pay=; level=; diamond=; brick=; is_check=; invite_code=; direct_pay=; alipay_name=; alipay_account=; type=; direct=; p_mobile=; p_wx=; p_qq=; p_qqq=; p_nickname=; money=',}
