1e5efb870b96e698be21a43d6028617153f926a6
xiaoyulr <106761309+xiaoyulr@users.noreply.github.com>
2022-07-31 08:22:09 +0800

    Update ydcdtx.py

--- ydcdtx.py
+++ ydcdtx.py
@@ -1,8 +1,4 @@
 """
-2abtest=8&appVersion=9.1.10&client=android&imei=a0226884-4cb4-487e-9278-3f1a730b5082&keyfrom=mdict.9.1.10.android&keyid=dict-usertask-key&mid=7.1.2&model=PCLM10&money=1&mysticTime=1652773976400&network=wifi&newImei=CQk4MDgzYjk3Zjg1OGRkNTBkCTAwYjc2MGI3&product=mdict&screen=1080x1920&ssid=airport&vendor=youdaoweb&vipfjnlouqgwkphlllg=wllirdrzzxfrywl&whnfnqdh=skktzfgdejca&key=ttfMFaa7tiPyAc3DanKeIMzEejm
-                                                                       abtest=9&appVersion=9.1.10&client=android&ddhrennrfzfls=khxjtbtzj&imei=0a09f99a9330d9e8435df1e0d466042d&keyfrom=mdict.9.1.10.android&keyid=dict-usertask-key&kvwlhphflgsl=ucjdujkis&mid=5.1.1&model=NX629J&money=1&mysticTime=1652781426641&network=wifi&newImei=010306022465495&product=mdict&screen=1080x1920&ssid=midea&vendor=youdaoweb&key=ttfMFaa7tiPyAc3DanKeIMzEejm : 937b5762e90c42d43147649eecb43e0d
-https://dict.youdao.com/dictusertask/withdraw/execute?mysticTime=1652781426641&kvwlhphflgsl=ucjdujkis&money=1&keyid=dict-usertask-key&sign=937b5762e90c42d43147649eecb43e0d&ddhrennrfzfls=khxjtbtzj&pointParam=ssid,screen,mid,money,keyid,newImei,mysticTime,network,appVersion,imei,keyfrom,client,kvwlhphflgsl,abtest,ddhrennrfzfls,vendor,product,model&network=wifi&ssid=midea&screen=1080x1920&appVersion=9.1.10&imei=0a09f99a9330d9e8435df1e0d466042d&mid=5.1.1&keyfrom=mdict.9.1.10.android&client=android&abtest=9&vendor=youdaoweb&newImei=010306022465495&model=NX629J&product=mdict
-"""
 import os
 import time
 
@@ -28,21 +24,27 @@ def withdraw(ck):
             'User-Agent': 'youdao_dict_android',
         }
         response = requests.get(url, headers=headers).json()
-#         print(response)
+        print(response)
         if response['msg']== 'OK':
             print(f"提现{response['data']['isWithdraw']}元成功还剩{response['data']['account']}元")
-            break
+            return
         elif response['msg'] == '提现金额超出账户余额':
-            break
+            print('目前余额不足')
+            return
+        elif response['msg'] == '该面值今日兑换额度已用完':
+            print('该面值今日兑换额度已用完')
+            return
         else:
             print(response['msg'])
-            pass
-        time.sleep(3)
+            return
+    time.sleep(3)
 
 
 if __name__ == '__main__':
 
     cks = os.environ['ydcd_data']
     cks = cks.split('@')
-    for ck in cks:
-        withdraw(ck)
+    for ck in range(len(cks)):
+        print(f"正在进行第{ck+1}账号提现")
+        withdraw(cks[ck])
+        print('\n')
