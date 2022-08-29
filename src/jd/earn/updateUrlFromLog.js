2ce578bf3a1c67bd252479ca55e32f694404a9da
jyhuang <326127285@qq.com>
2022-08-30 00:09:55 +0800

    update(jd/EarnAdvertPlugin): 更新活动
    - 调整自动更新脚本

--- src/jd/earn/updateUrlFromLog.js
+++ src/jd/earn/updateUrlFromLog.js
@@ -9,7 +9,7 @@ function main() {
   if (_.isEmpty(urls)) return console.log('没有需要更新的url');
   const logStr = getFileContent(getLogFile('app')).toString();
   const result = logStr.split('\n').filter(str => /快来一起领红包吧/.test(str) && /\[1]/.test(str) && /谢谢你哦，助力成功/.test(str));
-  const activityIds = result.map(str => str.match(/\[1] \[(\w+)]/)[1]);
+  const activityIds = result.map(str => str.match(/\[1] \[.*] \[(\w+)]/)[1]);
   urls = urls.filter(url => activityIds.includes(new URL(url).searchParams.get('activityId')));
   fs.writeFileSync(urlFile, urls.join('\n'));
   console.log(`未过期的活动: ${activityIds}, 已更新成功`);
