ee159f2ecbe56cdc61f06e9dca1fddc9059bbe52
jyhuang <326127285@qq.com>
2022-09-02 15:16:16 +0800

    perf(jd/EarnApplet): 增加助力逻辑

--- src/jd/cash/applet.js
+++ src/jd/cash/applet.js
@@ -2,6 +2,10 @@ const Template = require('../base/template');
 
 const {sleep, writeFileJSON, singleRun} = require('../../lib/common');
 const _ = require('lodash');
+const {getEnv} = require('../../lib/env');
+
+const shareCookieIndex = getEnv('JD_CASHAPPLET_SHARE_COOKIE_INDEX', 0, -1);
+const shareBodyData = {};
 
 class CashApplet extends Template {
   static scriptName = 'CashApplet';
@@ -10,7 +14,8 @@ class CashApplet extends Template {
   static shareCodeTaskList = [];
   static commonParamFn = () => ({});
   static activityEndTime = '';
-  static times = 1;
+  static doneShareTask = shareCookieIndex < 0;
+  static times = this.doneShareTask ? 1 : 2;
   static cookieKeys = ['wq_uin', 'wq_skey'];
 
   static apiOptions = {
@@ -51,7 +56,43 @@ class CashApplet extends Template {
 
           const result = [];
 
-          const taskList = _.property('data.result.taskInfos')(data) || [];
+          const {taskInfos: taskList, inviteCode, shareDate, limitTimeRedPacket} = _.get(data, 'data.result');
+
+          if (limitTimeRedPacket) {
+            let {canJoin, inStock, receiveStatus, redPacketList, redPacketProcess} = limitTimeRedPacket;
+            if (canJoin === '1' && inStock === '1') {
+              if (shareCookieIndex === api.currentCookieIndex) {
+                if (receiveStatus === '0') {
+                  // 开启
+                  await api.doFormBody('cash_join_limited_redpacket', _.pick(_.maxBy(redPacketList, 'level'), ['id', 'level'])).then(data => {
+                    self.isSuccess(data) && (receiveStatus = '1');
+                  });
+                }
+                if (receiveStatus === '1') {
+                  _.assign(shareBodyData, {inviteCode, shareDate});
+                  const _process = _.get(redPacketProcess, 'process', []);
+                  for (const {node, status} of _process) {
+                    if ([0/*未助力*/, 2/*已获取*/].includes(status)) continue;
+                    await api.doFormBody('cash_open_limited_redpacket', {node}).then(data => {
+                      self.isSuccess(data) && api.log(`获得 ${_.get(data, 'data.result.amountStr')}`);
+                    });
+                  }
+
+                  // 不需要助力
+                  if (!_.isEmpty(_process) && _.every(_process, ({status}) => status === 2)) {
+                    delete shareBodyData.inviteCode;
+                  }
+                }
+              }
+            }
+          }
+
+          if (shareBodyData.inviteCode && self.isLastLoop() && shareBodyData.inviteCode !== inviteCode) {
+            await api.doFormBody('redpack_limited_assist', shareBodyData).then(data => {
+              api.log(`${shareBodyData.inviteCode}[${shareCookieIndex}] ${_.get(data, 'data.result.limitTimeAssist.tips')}`);
+            });
+          }
+
           for (let {
             finishFlag: status,
             type,
