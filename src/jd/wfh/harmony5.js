bb980339145617ee2e79ba203cea569815cb7a43
jyhuang <326127285@qq.com>
2022-09-08 12:32:23 +0800

    update(jd/harmony5): 更新活动

--- src/jd/wfh/harmony5.js
+++ src/jd/wfh/harmony5.js
@@ -5,9 +5,9 @@ const {sleep, writeFileJSON} = require('../../lib/common');
 class Harmony5 extends HarmonyTemplate {
   static scriptNameDesc = '品牌闪购-惊喜盒子';
   static shareCodeTaskList = [];
-  static shareTaskId = 3;
+  static shareTaskId = -1;/*没有助力任务*/
   static skipTaskIds = [];
-  static commonParamFn = () => ({appId: '1EFRXxg'});
+  static commonParamFn = () => ({appId: '1ElZXwKuP'});
 }
 
 module.exports = Harmony5;
