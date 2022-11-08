bd640c419773d6f7ccf3118f7ee0fcf3350efed6
Pwolf-Lin <276104733@qq.com>
2022-11-08 10:54:36 +0800

    update

--- tebu.py
+++ tebu.py
@@ -38,7 +38,7 @@ class tebu:
             elif not res["Result"]["VipSignInDtl"]["IsSigInToday"]:
                 url = "https://wxa-tp.ezrpro.com/myvip/Vip/SignIn/SignIn"
                 data = json.dumps({
-                    "ActId": 789,
+                    "ActId": 792,
                     "ActRemindStatus": True
                 })
                 try:
